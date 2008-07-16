924951ff870f736c467329ad35dca32da882e627
wixner <wixner@660068cc-ad4d-0410-81c7-436461b1c8ff>
2008-07-16 09:41:16 +0000

    moved Mock stuff out of the test classes to be available for other projects too (eg the providertool does run tests on the same mock objects)

--- gbif-scheduler/src/main/java/org/gbif/scheduler/MockJob.java
+++ gbif-scheduler/src/main/java/org/gbif/scheduler/MockJob.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import org.gbif.scheduler.scheduler.Launchable;
 
 public class MockJob implements Launchable{
+	public static final String goodResult = "Run successfully";
+	public static String result = "not run yet";
 	private MockInstance mockInstance;
 	
 	private MockJob(MockInstance mockInstance) {
@@ -14,6 +16,7 @@ public class MockJob implements Launchable{
 
 	public void launch(Map<String, Object> seed) throws Exception {
 		System.out.println("MockJob launched successfully with DI MockInstance "+mockInstance.toString() + " and seed "+seed.toString());		
+		MockJob.result = goodResult;
 	}
 
 }
