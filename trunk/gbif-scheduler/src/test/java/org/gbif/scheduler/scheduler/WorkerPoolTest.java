9cffb684ee2813bef09dece26f31c413e8db36c4
wixner <wixner@660068cc-ad4d-0410-81c7-436461b1c8ff>
2008-07-16 15:01:13 +0000

    job includes repeatInDays property

---
+++ trunk/gbif-scheduler/src/test/java/org/gbif/scheduler/scheduler/WorkerPoolTest.java
@@ -0,0 +1,39 @@
+package org.gbif.scheduler.scheduler;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.appfuse.webapp.action.BaseActionTestCase;
+import org.gbif.scheduler.MockJob;
+import org.gbif.scheduler.model.Job;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WorkerPoolTest extends BaseActionTestCase{
+	private WorkerPool workerPool;	
+	
+	public void setWorkerPool(WorkerPool workerPool) {
+		this.workerPool = workerPool;
+	}
+
+
+	@Test
+	public void testScheduleNextRepeatingJob() {
+		Job job = new Job();
+		job.setDescription("test job");
+		job.setId(1L);
+		job.setDataAsJSON("{}");
+		job.setJobClassName(MockJob.class.getName());
+		job.setJobGroup("testGroup");
+		job.setNextFireTime(new Date());
+		
+		Job newJob = workerPool.getNextRepeatingJob(job);
+		assertNull(newJob);
+		// add repeating days to create a new job
+		job.setRepeatInDays(42);
+		newJob = workerPool.getNextRepeatingJob(job);
+		assertNotSame(job, newJob);
+	}
+
+}
