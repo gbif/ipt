d6cae4de682e702dede3915825fbdef4ebf95a5a
wixner <wixner@660068cc-ad4d-0410-81c7-436461b1c8ff>
2008-08-08 13:49:18 +0000

    nasty onchange select ajax call finally fixed using prototype

--- gbif-providertool/src/test/java/org/gbif/provider/util/GChartBuilderTest.java
+++ gbif-providertool/src/test/java/org/gbif/provider/util/GChartBuilderTest.java
@@ -1,49 +1,72 @@
 package org.gbif.provider.util;
 
-import static org.junit.Assert.fail;
-import static org.junit.Assert.*;
 
-import java.util.Date;
-import java.util.Calendar;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.List;
+import java.util.TreeMap;
 
+import org.gbif.provider.model.dto.StatsCount;
 import org.junit.Before;
 import org.junit.Test;
-import java.util.Arrays;
+
+import com.googlecode.gchartjava.GeographicalArea;
 
 public class GChartBuilderTest {
-	private UploadChartBuilder chartBuilder;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		chartBuilder = new UploadChartBuilder();
-		Long baseTime = 839977698718L;
-		Long dateTime;
-		Random rnd = new Random();
-		int datasetSize = rnd.nextInt(50);
-		for (String t : Arrays.asList("set1","set2","set3","set4")){
-			Map<Date, Long> dataset = new HashMap<Date, Long>();
-			for (int i=0; i<datasetSize; i++){
-				dateTime = 100000L * rnd.nextInt(2000000);
-				Long val = Long.valueOf(rnd.nextInt(1000));
-				if (t.equals("set4")){
-					val=val*10;
-				}
-				dataset.put(new Date(dateTime), val);
-			}
-			chartBuilder.addDataset(dataset, t);
-		}
 	}
 
 	@Test
-	public void testGenerateChartDataString() {
-		String result = chartBuilder.generateChartDataString(300,200);
+	public void testPie(){
+		GChartBuilder gb = new GChartBuilder();
+		List<StatsCount> data = new ArrayList<StatsCount>();
+		data.add(new StatsCount("Harald", 1012l));
+		data.add(new StatsCount("Stefan", 79l));
+		data.add(new StatsCount("Freerk", 891l));
+		data.add(new StatsCount("Fritz", 2122l));
+		data.add(new StatsCount("Bernd", 1422l));
+		Long totalRecords = 0l;
+		for (StatsCount val : data){
+			totalRecords += val.getCount();
+		}
+		String expectedString = "http://chart.apis.google.com/chart?cht=p&chs=320x160&chl=Harald|Stefan|Freerk|Fritz|Bernd&chts=000000,16&chco=76A4FB,D7E9F5,18427D,80C65A,CA3D05&chd=e:LhApKPYUQA&chtt=Phonecalls+per+Friend";
+		String result = gb.generatePiaChartUrl(320, 160, "Phonecalls per Friend", data, totalRecords);
 //		System.out.println(result);
-		chartBuilder.clear();
-		result = chartBuilder.generateChartDataString(300,200);
+        assertEquals(expectedString, result);
+        
+		expectedString = "http://chart.apis.google.com/chart?cht=p&chs=320x160&chl=Harald|Stefan|Freerk|Fritz|Bernd&chts=000000,16&chco=76A4FB,D7E9F5,18427D,80C65A,CA3D05&chd=e:LhApKPYUQA";
+		result = gb.generatePiaChartUrl(320, 160, data, totalRecords);
 //		System.out.println(result);
+        assertEquals(expectedString, result);
 	}
+	
+	@Test
+	public void testMap(){
+		GChartBuilder gb = new GChartBuilder();
+		List<StatsCount> data = new ArrayList<StatsCount>();
+		data.add(new StatsCount("DE", 1012l));
+		data.add(new StatsCount("UK", 790l));
+		data.add(new StatsCount("FR", 1291l));
+		data.add(new StatsCount("AR", 8222l));
+		data.add(new StatsCount("BR", 12122l));
+		data.add(new StatsCount("DA", 522l));
+		data.add(new StatsCount("RU", 1342l));
+		data.add(new StatsCount("Belgium", 18842l));
+		String expectedString = "http://chart.apis.google.com/chart?cht=t&chtm=world&chs=440x220&chld=DEUKFRARBRDARU&chco=FFFFFF,EDF0D4,13390D&chf=bg,s,E0F2FF&chd=e:DNCkD2bho9BSEf";
+		String result = gb.generateMapChartUrl(440, 220, data);
+//		System.out.println(result);
+        assertEquals(expectedString, result);
+	}	
 
+	@Test
+	public void testGetArea(){
+		GeographicalArea a;
+		a = GeographicalArea.valueOf("SOUTH_AMERICA");
+		a = GeographicalArea.valueOf("south_america".toUpperCase());
+		System.out.println(a);
+	}
 }
