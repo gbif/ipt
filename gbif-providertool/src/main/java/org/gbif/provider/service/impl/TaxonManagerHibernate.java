6d2bb6abfee2de4b4a4fd8ab1b89a8ab8358379c
wixner <wixner@660068cc-ad4d-0410-81c7-436461b1c8ff>
2008-08-15 10:06:36 +0000

    Taxonomy builder tests running again. Also removing all previously existing taxa for the generated resource now

--- gbif-providertool/src/main/java/org/gbif/provider/service/impl/TaxonManagerHibernate.java
+++ gbif-providertool/src/main/java/org/gbif/provider/service/impl/TaxonManagerHibernate.java
@@ -3,8 +3,10 @@ package org.gbif.provider.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.gbif.provider.model.OccurrenceResource;
 import org.gbif.provider.model.Taxon;
 import org.gbif.provider.service.TaxonManager;
+import org.hibernate.Session;
 
 public class TaxonManagerHibernate extends GenericManagerHibernate<Taxon> implements TaxonManager {
 
@@ -30,6 +32,16 @@ public class TaxonManagerHibernate extends GenericManagerHibernate<Taxon> implem
 	public List<Taxon> getRoots(Long resourceId) {
 		List<Taxon> result = new ArrayList<Taxon>(); 
 		return result;
+	}
+
+	public void deleteAll(OccurrenceResource resource) {
+		// use DML-style HQL batch updates
+		// http://www.hibernate.org/hib_docs/reference/en/html/batch.html
+		Session session = getSession();
+		String hqlUpdate = String.format("delete Taxon tax WHERE tax.resource = :resource", persistentClass.getName());
+		int count = session.createQuery( hqlUpdate )
+		        .setEntity("resource", resource)
+		        .executeUpdate();
 	}
 
 
