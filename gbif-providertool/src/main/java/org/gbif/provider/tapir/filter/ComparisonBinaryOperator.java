458fd8f037b1f89e63eacbcee84d6527c63554d5
timrobertson100 <timrobertson100@660068cc-ad4d-0410-81c7-436461b1c8ff>
2009-01-19 16:49:36 +0000

    Filter parameter replacement working

--- gbif-providertool/src/main/java/org/gbif/provider/tapir/filter/ComparisonBinaryOperator.java
+++ gbif-providertool/src/main/java/org/gbif/provider/tapir/filter/ComparisonBinaryOperator.java
@@ -1,5 +1,7 @@
 package org.gbif.provider.tapir.filter;
 
+import java.util.Map;
+
 public abstract class ComparisonBinaryOperator extends ComparisonOperator{
 	protected String value;
 	public String getValue() {
@@ -7,6 +9,23 @@ public abstract class ComparisonBinaryOperator extends ComparisonOperator{
 	}
 	public void setValue(String value) {
 		this.value = value;
+	}
+	/**
+	 * Sets the value to be the first value found from the params map with the specified key
+	 * @param params the parameters to read from
+	 * @param key For the map
+	 */
+public void setValue(Map params, String key) {
+		String[] values = (String[])params.get(key);
+		if (values!=null && values.length==1) {
+			this.value = values[0];
+		} else if (values!=null && values.length>1) {
+			log.info("Using the first value for the filter from the options[" + values + "]");
+			this.value = values[0];
+		} else {
+			log.warn("No values in the parameters[" + params + "] for the key[" + key + "]");
+			this.value="ERROR: No values for key[" + key + "]";
+		}
 	}
 	
 	public String toHQL() {
