370a7849927376c605e2a20d7acce8ff1d6e1bd5
timrobertson100 <timrobertson100@660068cc-ad4d-0410-81c7-436461b1c8ff>
2009-01-21 22:53:56 +0000

    removed redundant import

--- trunk/gbif-providertool/src/main/java/org/gbif/provider/util/XmlFileUtils.java
+++ trunk/gbif-providertool/src/main/java/org/gbif/provider/util/XmlFileUtils.java
@@ -2,11 +2,9 @@ package org.gbif.provider.util;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 
 public class XmlFileUtils {
