aa518849e2a037832ac11d93e09755bdc5b28189
wixner <wixner@660068cc-ad4d-0410-81c7-436461b1c8ff>
2009-01-23 19:55:46 +0000

    fixed extension db entries for geosptail

--- trunk/gbif-providertool/src/test/java/org/gbif/provider/util/XmlContentHandlerTest.java
+++ trunk/gbif-providertool/src/test/java/org/gbif/provider/util/XmlContentHandlerTest.java
@@ -1,27 +1,30 @@
 package org.gbif.provider.util;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.net.URL;
 
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.gbif.provider.tapir.TemplateFactoryTest;
 import org.junit.Test;
+import org.xml.sax.SAXException;
+
 import static org.junit.Assert.assertTrue;
 
 
 public class XmlContentHandlerTest {
 	@Test
-	public void testHandler(){
+	public void testHandler() throws ParserConfigurationException, SAXException, IOException{
 		 SAXParserFactory factory = SAXParserFactory.newInstance();
-		  try {
-		      SAXParser saxParser = factory.newSAXParser();
-		      XmlContentHandler handler = new XmlContentHandler();
-		      saxParser.parse( new File("/Users/markus/Desktop/eml.xml"),  handler);
-		      assertTrue(handler.getContent().length()>100);
-		  } catch (Throwable err) {
-		        err.printStackTrace ();
-		  }		
+	      SAXParser saxParser = factory.newSAXParser();
+	      XmlContentHandler handler = new XmlContentHandler();
+	      File eml = new File(XmlContentHandlerTest.class.getResource("/eml.xml").getFile());
+	      saxParser.parse(eml,  handler);
+	      assertTrue(handler.getContent().length()>100);
 	}
 }
