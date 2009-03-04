58f8b48a3d6500afddbb8c919131dbae29869349
timrobertson100 <timrobertson100@660068cc-ad4d-0410-81c7-436461b1c8ff>
2009-03-04 11:12:21 +0000

    Working tests and full parsing of Thesaurus terms

--- gbif-providertool/src/test/java/org/gbif/provider/service/util/ThesaurusFactoryTest.java
+++ gbif-providertool/src/test/java/org/gbif/provider/service/util/ThesaurusFactoryTest.java
@@ -2,8 +2,11 @@ package org.gbif.provider.service.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import org.gbif.provider.model.ThesaurusConcept;
+import org.gbif.provider.model.ThesaurusTerm;
 import org.gbif.provider.model.ThesaurusVocabulary;
 import org.junit.Test;
 
@@ -18,6 +21,23 @@ public class ThesaurusFactoryTest {
 			
 			assertNotNull(tv.getConcepts());
 			assertEquals(7694, tv.getConcepts().size());
+			
+			ThesaurusConcept tc = tv.getConcepts().get(0);
+			assertEquals("aaa", tc.getIdentifier());
+			assertEquals("http://vocabularies.gbif.org/lang/aaa", tc.getLink());
+			assertEquals("http://www.sil.org/iso639-3/documentation.asp?id=aaa", tc.getUri());
+			assertNotNull(tc.getIssued());
+			assertEquals(tv, tc.getVocabulary());
+			
+			assertNotNull(tc.getTerms());
+			assertEquals(1, tc.getTerms().size());
+			ThesaurusTerm tt = tc.getTerms().iterator().next();
+			assertEquals(tc, tt.getConcept());
+			assertNotNull(tt.getCreated());
+			assertNotNull(tt.getModified());
+			assertEquals("en", tt.getLang());
+			assertTrue(tt.isPreferred());
+			assertEquals("Ghotuo", tt.getTitle());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
