f950322adaea272a8edd1150b5e66d9987b4296f
wixner <wixner@660068cc-ad4d-0410-81c7-436461b1c8ff>
2009-09-08 07:08:20 +0000

    exploded appfuse source code, removed some unused bits like xfire & multiple themes and tried to clean up merged code. Project compiles again, but the webapp doesnt run seamlessly. The displaytag library isnt loaded into freemarker properly.

---
+++ trunk/gbif-providertool/src/main/webapp/WEB-INF/pages/dataAccessFailure.jsp
@@ -0,0 +1,21 @@
+<%@ include file="/common/taglibs.jsp" %>
+
+<title>Data Access Error</title>
+
+<head>
+    <meta name="heading" content="Data Access Failure"/>
+    <meta name="menu" content="AdminMenu"/>
+</head>
+
+<p>
+    <c:out value="${requestScope.exception.message}"/>
+</p>
+
+<!--
+<% 
+Exception ex = (Exception) request.getAttribute("exception");
+ex.printStackTrace(new java.io.PrintWriter(out)); 
+%>
+-->
+
+<a href="mainMenu.html" onclick="history.back();return false">&#171; Back</a>
---
+++ trunk/gbif-providertool/src/test/java/org/appfuse/dao/BaseDaoTestCase.java
@@ -0,0 +1,91 @@
+package org.appfuse.dao;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
+
+/**
+ * Base class for running DAO tests.
+ * @author mraible
+ */
+public abstract class BaseDaoTestCase extends AbstractTransactionalDataSourceSpringContextTests {
+    /**
+     * Log variable for all child classes. Uses LogFactory.getLog(getClass()) from Commons Logging
+     */
+    protected final Log log = LogFactory.getLog(getClass());
+    /**
+     * ResourceBundle loaded from src/test/resources/${package.name}/ClassName.properties (if exists)
+     */
+    protected ResourceBundle rb;
+
+    /**
+     * Sets AutowireMode to AUTOWIRE_BY_NAME and configures all context files needed to tests DAOs.
+     * @return String array of Spring context files.
+     */
+    protected String[] getConfigLocations() {
+        setAutowireMode(AUTOWIRE_BY_NAME);
+        return new String[] {
+                "classpath:/applicationContext-resources.xml",
+                "classpath:/applicationContext-dao.xml",
+                "classpath*:/applicationContext.xml", // for modular projects
+                "classpath:**/applicationContext*.xml" // for web projects
+            };
+    }
+
+    /**
+     * Default constructor - populates "rb" variable if properties file exists for the class in
+     * src/test/resources.
+     */
+    public BaseDaoTestCase() {
+        // Since a ResourceBundle is not required for each class, just
+        // do a simple check to see if one exists
+        String className = this.getClass().getName();
+
+        try {
+            rb = ResourceBundle.getBundle(className);
+        } catch (MissingResourceException mre) {
+            //log.warn("No resource bundle found for: " + className);
+        }
+    }
+
+    /**
+     * Utility method to populate a javabean-style object with values
+     * from a Properties file
+     * @param obj the model object to populate
+     * @return Object populated object
+     * @throws Exception if BeanUtils fails to copy properly
+     */
+    protected Object populate(Object obj) throws Exception {
+        // loop through all the beans methods and set its properties from its .properties file
+        Map<String, String> map = new HashMap<String, String>();
+
+        for (Enumeration<String> keys = rb.getKeys(); keys.hasMoreElements();) {
+            String key = keys.nextElement();
+            map.put(key, rb.getString(key));
+        }
+
+        BeanUtils.copyProperties(map, obj);
+
+        return obj;
+    }
+
+    /**
+     * Create a HibernateTemplate from the SessionFactory and call flush() and clear() on it.
+     * Designed to be used after "save" methods in tests: http://issues.appfuse.org/browse/APF-178.
+     */
+    protected void flush() {
+        HibernateTemplate hibernateTemplate =
+                new HibernateTemplate((SessionFactory) applicationContext.getBean("sessionFactory"));
+        hibernateTemplate.flush();
+        hibernateTemplate.clear();
+    }
+}
