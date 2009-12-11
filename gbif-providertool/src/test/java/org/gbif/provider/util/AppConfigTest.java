cdd0e59f702555be7ce9e629d75f50eba99a9467
eightysteele <eightysteele@660068cc-ad4d-0410-81c7-436461b1c8ff>
2009-12-11 04:10:26 +0000

    Initial import of baseline unit testing for modifications that support new Darwin Core.

--- gbif-providertool/src/test/java/org/gbif/provider/util/AppConfigTest.java
+++ gbif-providertool/src/test/java/org/gbif/provider/util/AppConfigTest.java
@@ -15,10 +15,8 @@
  */
 package org.gbif.provider.util;
 
-import org.gbif.provider.model.CoreRecord;
-import org.gbif.provider.model.DarwinCore;
-import org.gbif.provider.model.OccurrenceResource;
 import org.gbif.provider.model.factory.DarwinCoreFactory;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -26,31 +24,24 @@ import org.springframework.beans.factory.annotation.Autowired;
  * TODO: Documentation.
  * 
  */
-public class AppConfigTest /* extends ContextAwareTestBase */{
+public class AppConfigTest extends ContextAwareTestBase {
   @Autowired
   public AppConfig cfg;
   @Autowired
   private DarwinCoreFactory dwcFactory;
 
   @Test
-  public void testCoreDetail() {
-    CoreRecord rec = DarwinCore.newMock(new OccurrenceResource());
-    System.out.println(cfg.getDetailUrl(rec));
-    System.out.println(cfg.getDetailUrl(rec, "xml"));
-  }
-
-  @Test
   public void testPropertiesLoaded() {
-    // assertEquals("http://localhost:8080/ipt", cfg.getBaseUrl());
+    assertEquals("http://localhost:8080/ipt", cfg.getBaseUrl());
   }
 
   @Test
   public void testSetAppBaseUrl() {
     cfg.setBaseUrl("http://localhost:8080/ipt/");
-    // assertEquals("http://localhost:8080/ipt", cfg.getBaseUrl());
+    assertEquals("http://localhost:8080/ipt", cfg.getBaseUrl());
 
     cfg.setBaseUrl(" http://localhost:8080/ipt  ");
-    // assertEquals("http://localhost:8080/ipt", cfg.getBaseUrl());
+    assertEquals("http://localhost:8080/ipt", cfg.getBaseUrl());
   }
 
 }
