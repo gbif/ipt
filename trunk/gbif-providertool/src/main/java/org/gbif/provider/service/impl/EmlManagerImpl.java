64dc761efe94b7f2cc490f3977a851e6fb7f6c13
eightysteele <eightysteele@660068cc-ad4d-0410-81c7-436461b1c8ff>
2010-02-02 16:59:42 +0000

    Fixes issue 263. Modifies the load method to create a new Eml object and write it to XML when the resource is new or when an existing EML file cannot be read.

--- trunk/gbif-providertool/src/main/java/org/gbif/provider/service/impl/EmlManagerImpl.java
+++ trunk/gbif-providertool/src/main/java/org/gbif/provider/service/impl/EmlManagerImpl.java
@@ -15,42 +15,75 @@
  */
 package org.gbif.provider.service.impl;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.gbif.provider.util.XmlFileUtils.getUtf8Reader;
+import static org.gbif.provider.util.XmlFileUtils.startNewUtf8XmlFile;
+import static org.springframework.ui.freemarker.FreeMarkerTemplateUtils.processTemplateIntoString;
+
 import org.gbif.provider.model.Resource;
 import org.gbif.provider.model.eml.Eml;
 import org.gbif.provider.service.EmlManager;
 import org.gbif.provider.service.GenericResourceManager;
 import org.gbif.provider.util.AppConfig;
-import org.gbif.provider.util.XmlFileUtils;
 
-import com.thoughtworks.xstream.XStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import com.thoughtworks.xstream.XStream;
 
 import freemarker.template.Configuration;
 import freemarker.template.TemplateException;
 
 /**
- * TODO: Documentation
- * 
+ * This class can be used to load and save {@link Eml} objects and publish new
+ * EML versions for specific resources.
  * 
  */
 public class EmlManagerImpl implements EmlManager {
+
   protected static final Log log = LogFactory.getLog(EmlManagerImpl.class);
+
+  // TODO: Should this be moved into Constants?
   private static final String EML_TEMPLATE = "/WEB-INF/pages/eml.ftl";
+
+  /**
+   * Writes an {@link Eml} object to an XML file using a Freemarker
+   * {@link Configuration}. Returns true if the XML file is succesfully written
+   * and false otherwise.
+   * 
+   * @param f the XML file to write to
+   * @param c the Freemarker configuration
+   * @param eml the EML object
+   * @return true if EML is written to XML, false otherwise
+   */
+  private static boolean writeEmlXmlFile(File f, Configuration c, Eml eml) {
+    try {
+      checkNotNull(f, "XML file was null");
+      checkNotNull(c, "Freemarker configuration was null");
+      checkNotNull(eml, "Eml object was null");
+      Map<String, Object> map = new HashMap<String, Object>();
+      map.put("eml", eml);
+      String data = processTemplateIntoString(c.getTemplate(EML_TEMPLATE), map);
+      Writer out = startNewUtf8XmlFile(f);
+      out.write(data);
+      out.close();
+      return true;
+    } catch (Exception e) {
+      log.error(e.toString());
+      return false;
+    }
+  }
 
   @Autowired
   public AppConfig cfg;
@@ -64,35 +97,37 @@ public class EmlManagerImpl implements EmlManager {
 
   private final XStream xstream = new XStream();
 
+  /**
+   * Loads and returns an {@link Eml} object for a {@link Resource} by reading
+   * in the metadata XML file specified by {@link AppConfig}.
+   * 
+   * If the resource is not yet persistent or if there is an error reading the
+   * XML file, a new Eml object is created, written to XML, and then returned.
+   */
+  @SuppressWarnings("static-access")
   public Eml load(Resource resource) {
-    if (resource == null) {
-      throw new NullPointerException("EML file requires resource");
-    }
-    Eml metadata = null;
-    if (resource.getId() != null) {
-      // load existing data. This is a persistent resource
-      File metadataFile = cfg.getMetadataFile(resource.getId());
+    checkNotNull(resource, "Resource was null");
+    Eml eml = null;
+    Long rid = resource.getId();
+    if (rid != null) {
+      File f = cfg.getMetadataFile(rid);
       try {
-        Reader reader = XmlFileUtils.getUtf8Reader(metadataFile);
-        metadata = (Eml) xstream.fromXML(reader);
+        eml = (Eml) xstream.fromXML(getUtf8Reader(f));
       } catch (FileNotFoundException e) {
-        log.error(String.format("EML Metadata file not found for resource %s",
-            resource.getId()));
+        log.error(String.format("%s not found for resource %s", f, rid));
+        log.info(String.format("Creating new Eml object for resource %s", rid));
+        eml = new Eml();
+        eml.setResource(resource);
+        eml.getResourceCreator().setEmail(resource.getContactEmail());
+        eml.getResourceCreator().setLastName(resource.getContactName());
+        eml.setPubDate(new Date());
+        writeEmlXmlFile(cfg.getEmlFile(resource.getId()), freemarker, eml);
       }
     }
-    if (metadata == null) {
-      // file not found or new resource
-      metadata = new Eml();
-      // copy some resource default data that is not delegated (title &
-      // description are)
-      metadata.getResourceCreator().setEmail(resource.getContactEmail());
-      metadata.getResourceCreator().setLastName(resource.getContactName());
-    }
-
-    metadata.setResource(resource);
-    return metadata;
+    return eml;
   }
 
+  @SuppressWarnings("static-access")
   public Eml publishNewEmlVersion(Resource resource) throws IOException {
     Eml metadata = load(resource);
     int version = metadata.increaseEmlVersion();
@@ -102,9 +137,9 @@ public class EmlManagerImpl implements EmlManager {
       File currEmlFile = cfg.getEmlFile(resource.getId());
       Map<String, Object> data = new HashMap<String, Object>();
       data.put("eml", metadata);
-      String eml = FreeMarkerTemplateUtils.processTemplateIntoString(
+      String eml = processTemplateIntoString(
           freemarker.getTemplate(EML_TEMPLATE), data);
-      Writer out = XmlFileUtils.startNewUtf8XmlFile(currEmlFile);
+      Writer out = startNewUtf8XmlFile(currEmlFile);
       out.write(eml);
       out.close();
       // also create archived fixed version
@@ -122,15 +157,18 @@ public class EmlManagerImpl implements EmlManager {
   }
 
   /**
-   * serialises the metadata document into a file using XMLStream?
+   * Serializes this manager's {@link Eml} object into an XML file. The location
+   * and name of this file is specified by {@link AppConfig}.
+   * 
    */
+  @SuppressWarnings("static-access")
   public void save(Eml metadata) {
     // update persistent EML properties on resource
     Resource res = metadata.getResource();
     // now persist EML file (resource must have ID now)
     try {
       File metadataFile = cfg.getMetadataFile(res.getId());
-      Writer writer = XmlFileUtils.startNewUtf8XmlFile(metadataFile);
+      Writer writer = startNewUtf8XmlFile(metadataFile);
       xstream.toXML(metadata, writer);
       writer.close();
     } catch (IOException ex) {
