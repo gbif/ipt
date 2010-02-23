57eff38f58c39c230865c9308599dc0124e445ef
eightysteele <eightysteele@660068cc-ad4d-0410-81c7-436461b1c8ff>
2010-02-23 20:26:17 +0000

    Added the TaxonomicCoverage class for modeling taxonomic coverage attributes. Designed to support the GBIF Extended Metadata Profile. Also added the TaxonomicCoverageTest class for related unit testing.

---
+++ gbif-providertool/src/test/java/org/gbif/provider/model/eml/TaxonomicCoverageTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2009 GBIF.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.gbif.provider.model.eml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+/**
+ * This class can be used for unit testing {@link TaxonomicCoverage}.
+ * 
+ */
+public class TaxonomicCoverageTest {
+
+  @Test
+  public final void tesirreate() {
+    TaxonomicCoverage.create("d", null, null);
+    Set<TaxonKeyword> keywords = Sets.newHashSet();
+    TaxonomicCoverage.create("d", "ir", keywords);
+  }
+
+  @Test
+  public final void testEqualsObject() {
+    Set<TaxonKeyword> keywords = Sets.newHashSet();
+    assertEquals(TaxonomicCoverage.create("d", "ir", keywords),
+        TaxonomicCoverage.create("d", "ir", keywords));
+  }
+
+  @Test
+  public final void testGetDescription() {
+    Set<TaxonKeyword> keywords = Sets.newHashSet();
+    assertEquals("d",
+        TaxonomicCoverage.create("d", "ir", keywords).getDescription());
+  }
+
+  @Test
+  public final void testGetIdReference() {
+    Set<TaxonKeyword> keywords = Sets.newHashSet();
+    assertEquals("ir",
+        TaxonomicCoverage.create("d", "ir", keywords).getIdReference());
+  }
+
+  @Test
+  public final void testGetKeywords() {
+    Set<TaxonKeyword> keywords = Sets.newHashSet();
+    assertEquals(keywords,
+        TaxonomicCoverage.create("d", "ir", keywords).getKeywords());
+  }
+
+  @Test
+  public final void testHashCode() {
+    Set<TaxonKeyword> keywords = Sets.newHashSet();
+    assertEquals(TaxonomicCoverage.create("d", "ir", keywords).hashCode(),
+        TaxonomicCoverage.create("d", "ir", keywords).hashCode());
+    TaxonomicCoverage tc = TaxonomicCoverage.create("d", "ir", keywords);
+    Map<TaxonomicCoverage, String> map = Maps.newHashMap();
+    map.put(tc, "foo");
+    assertTrue(map.containsKey(TaxonomicCoverage.create("d", "ir", keywords)));
+  }
+
+  @Test
+  public final void testToString() {
+    Set<TaxonKeyword> keywords = Sets.newHashSet();
+    TaxonomicCoverage tc = TaxonomicCoverage.create("d", "ir", keywords);
+    assertEquals("Description=d, IdReference=ir, Keywords=[[]]", tc.toString());
+  }
+}
