3d5c04d152a5cccda02e9df7d512bec972f3186c
eightysteele <eightysteele@660068cc-ad4d-0410-81c7-436461b1c8ff>
2010-04-13 19:04:11 +0000

    Code style changes only. No functional diff.

--- gbif-providertool/src/main/java/org/gbif/provider/model/eml/Method.java
+++ gbif-providertool/src/main/java/org/gbif/provider/model/eml/Method.java
@@ -21,101 +21,114 @@ import java.io.Serializable;
  * Encapsulates the description of the sampling methods employed
  */
 public class Method implements Serializable {
-	/**
-	 * Generated 
-	 */
-	private static final long serialVersionUID = 8272714768039859733L;
+  /**
+   * Generated
+   */
+  private static final long serialVersionUID = 8272714768039859733L;
 
-	/**
-	 * Default constructor required by Struts2
-	 */
-	public Method() {
-	}
+  /**
+   * "The methodStep field allows for repeated sets of elements that document a
+   * series of procedures followed to produce a data object. These include text
+   * descriptions of the procedures, relevant literature, software,
+   * instrumentation, source data and any quality control measures taken." This
+   * implementation allows only the declaration of the step description
+   * 
+   * @see http://knb.ecoinformatics.org/software/eml/eml-2.1.0/eml-methods.html#
+   *      methodStep
+   */
+  private String stepDescription;
 
-	/**
-	 * "The methodStep field allows for repeated sets of elements that document a series of procedures followed to produce a data object. 
-	 * These include text descriptions of the procedures, relevant literature, software, instrumentation, source data 
-	 * and any quality control measures taken."
-	 * This implementation allows only the declaration of the step description 
-	 * @see http://knb.ecoinformatics.org/software/eml/eml-2.1.0/eml-methods.html#methodStep
-	 */
-	private String stepDescription;
-	
-	/**
-	 * "The coverage field allows for a textual description of the specific sampling area, the sampling frequency (temporal boundaries, 
-	 * frequency of occurrence), and groups of living organisms sampled (taxonomic coverage)."
-	 * This implementation allows only the declaration of the extent description   
-	 * @see http://knb.ecoinformatics.org/software/eml/eml-2.1.0/eml-methods.html#studyExtent
-	 */
-	private String studyExtent;
-	
-	/**
-	 * The samplingDescription field allows for a text-based/human readable description of the sampling procedures used in the research project. 
-	 * The content of this element would be similar to a description of sampling procedures found in the methods section of a journal article. 
-	 * @see http://knb.ecoinformatics.org/software/eml/eml-2.1.0/eml-methods.html#samplingDescription
-	 */
-	private String sampleDescription;
-	
-	/**
-	 * The qualityControl field provides a location for the description of actions taken to either control or assess the quality of 
-	 * data resulting from the associated method step. 
-	 * @see http://knb.ecoinformatics.org/software/eml/eml-2.1.0/eml-methods.html#qualityControl
-	 */
-	private String qualityControl;
+  /**
+   * "The coverage field allows for a textual description of the specific
+   * sampling area, the sampling frequency (temporal boundaries, frequency of
+   * occurrence), and groups of living organisms sampled (taxonomic coverage)."
+   * This implementation allows only the declaration of the extent description
+   * 
+   * @see http://knb.ecoinformatics.org/software/eml/eml-2.1.0/eml-methods.html#
+   *      studyExtent
+   */
+  private String studyExtent;
 
-	/**
-	 * @return the stepDescription
-	 */
-	public String getStepDescription() {
-		return stepDescription;
-	}
+  /**
+   * The samplingDescription field allows for a text-based/human readable
+   * description of the sampling procedures used in the research project. The
+   * content of this element would be similar to a description of sampling
+   * procedures found in the methods section of a journal article.
+   * 
+   * @see http://knb.ecoinformatics.org/software/eml/eml-2.1.0/eml-methods.html#
+   *      samplingDescription
+   */
+  private String sampleDescription;
 
-	/**
-	 * @param stepDescription the stepDescription to set
-	 */
-	public void setStepDescription(String stepDescription) {
-		this.stepDescription = stepDescription;
-	}
+  /**
+   * The qualityControl field provides a location for the description of actions
+   * taken to either control or assess the quality of data resulting from the
+   * associated method step.
+   * 
+   * @see http://knb.ecoinformatics.org/software/eml/eml-2.1.0/eml-methods.html#
+   *      qualityControl
+   */
+  private String qualityControl;
 
-	/**
-	 * @return the studyExtent
-	 */
-	public String getStudyExtent() {
-		return studyExtent;
-	}
+  /**
+   * Default constructor required by Struts2
+   */
+  public Method() {
+  }
 
-	/**
-	 * @param studyExtent the studyExtent to set
-	 */
-	public void setStudyExtent(String studyExtent) {
-		this.studyExtent = studyExtent;
-	}
+  /**
+   * @return the qualityControl
+   */
+  public String getQualityControl() {
+    return qualityControl;
+  }
 
-	/**
-	 * @return the sampleDescription
-	 */
-	public String getSampleDescription() {
-		return sampleDescription;
-	}
+  /**
+   * @return the sampleDescription
+   */
+  public String getSampleDescription() {
+    return sampleDescription;
+  }
 
-	/**
-	 * @param sampleDescription the sampleDescription to set
-	 */
-	public void setSampleDescription(String sampleDescription) {
-		this.sampleDescription = sampleDescription;
-	}
+  /**
+   * @return the stepDescription
+   */
+  public String getStepDescription() {
+    return stepDescription;
+  }
 
-	/**
-	 * @return the qualityControl
-	 */
-	public String getQualityControl() {
-		return qualityControl;
-	}
+  /**
+   * @return the studyExtent
+   */
+  public String getStudyExtent() {
+    return studyExtent;
+  }
 
-	/**
-	 * @param qualityControl the qualityControl to set
-	 */
-	public void setQualityControl(String qualityControl) {
-		this.qualityControl = qualityControl;
-	}
+  /**
+   * @param qualityControl the qualityControl to set
+   */
+  public void setQualityControl(String qualityControl) {
+    this.qualityControl = qualityControl;
+  }
+
+  /**
+   * @param sampleDescription the sampleDescription to set
+   */
+  public void setSampleDescription(String sampleDescription) {
+    this.sampleDescription = sampleDescription;
+  }
+
+  /**
+   * @param stepDescription the stepDescription to set
+   */
+  public void setStepDescription(String stepDescription) {
+    this.stepDescription = stepDescription;
+  }
+
+  /**
+   * @param studyExtent the studyExtent to set
+   */
+  public void setStudyExtent(String studyExtent) {
+    this.studyExtent = studyExtent;
+  }
 }
