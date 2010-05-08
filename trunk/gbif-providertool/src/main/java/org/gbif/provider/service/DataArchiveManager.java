9ab5c1624ad207d496c4828393a9cefd9d156f81
eightysteele <eightysteele@660068cc-ad4d-0410-81c7-436461b1c8ff>
2010-05-08 23:30:20 +0000

    This commit enables DataArchiveManagerImpl to open all supported Darwin Core Archive types, including archives that are compressed using ZIP and GZIP. The implementation is based on the 1.1 dwc-archive-reader library that is deployed to the GBIF Maven repository. This commit also adds unit testing coverage and example archives.

--- trunk/gbif-providertool/src/main/java/org/gbif/provider/service/DataArchiveManager.java
+++ trunk/gbif-providertool/src/main/java/org/gbif/provider/service/DataArchiveManager.java
@@ -18,6 +18,9 @@ package org.gbif.provider.service;
 import org.gbif.dwc.text.Archive;
 import org.gbif.dwc.text.UnsupportedArchiveException;
 import org.gbif.provider.model.DataResource;
+import org.gbif.provider.model.SourceFile;
+
+import com.google.common.collect.ImmutableSet;
 
 import java.io.File;
 import java.io.IOException;
@@ -30,19 +33,39 @@ import java.io.IOException;
  * 
  */
 public interface DataArchiveManager {
+
+  /**
+   * Creates a Darwin Core Archive on disk for a {@link DataResource}.
+   * 
+   * @see http://goo.gl/Xbnr
+   * 
+   * @param resource the resource to archive
+   * @return the archived file
+   * @throws IOException
+   */
   File createArchive(DataResource resource) throws IOException;
 
   /**
-   * Opens an existing archive file that is stored using the Darwin Core Archive
-   * format and returns an {@link Archive}
+   * Returns an immutable set of core {@link SourceFile} associated with an
+   * {@link Archive}.
+   * 
+   * @param archive the archive
+   * @return SourceFile an immutable set of core source files
+   */
+  ImmutableSet<SourceFile> getCoreSourceFiles(Archive archive);
+
+  /**
+   * Opens an existing archive file that is stored on disk using the Darwin Core
+   * Archive format and returns it as an {@link Archive}.
    * 
    * @see http://goo.gl/Xbnr
    * 
    * @param location the file location of the archive
+   * @param normaliseExtension
    * @return {@link Archive}
    * @throws IOException
    * @throws UnsupportedArchiveException
    */
-  Archive openArchive(File location) throws IOException,
-      UnsupportedArchiveException;
+  Archive openArchive(File location, boolean normaliseExtension)
+      throws IOException, UnsupportedArchiveException;
 }
