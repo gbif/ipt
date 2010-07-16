e074002dfe3531678c555ff4ee69b30d3ea5011a
eightysteele <eightysteele@660068cc-ad4d-0410-81c7-436461b1c8ff>
2010-07-16 16:58:37 +0000

    Create org unit test.

--- trunk/gbif-providertool/src/test/java/org/gbif/provider/service/impl/RegistryManagerTest.java
+++ trunk/gbif-providertool/src/test/java/org/gbif/provider/service/impl/RegistryManagerTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010 Regents of the University of California, University of Kansas.
+ * Copyright 2010 GBIF.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,213 +15,280 @@
  */
 package org.gbif.provider.service.impl;
 
-import static org.junit.Assert.*;
-
+import org.apache.commons.httpclient.HttpStatus;
+import org.gbif.provider.service.RegistryManager;
+import org.gbif.provider.util.ContextAwareTestBase;
+import org.gbif.registry.api.client.GbrdsOrganisation;
+import org.gbif.registry.api.client.Gbrds.OrgCredentials;
+import org.gbif.registry.api.client.GbrdsRegistry.CreateOrgResponse;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  */
-public class RegistryManagerTest {
+public class RegistryManagerTest extends ContextAwareTestBase {
+
+  @Autowired
+  RegistryManager registry;
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#createOrg(org.gbif.registry.api.client.GbrdsOrganisation)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#createOrg(org.gbif.registry.api.client.GbrdsOrganisation)}
+   * .
    */
   @Test
   public final void testCreateOrg() {
-    fail("Not yet implemented"); // TODO
+    String name = "Name" + System.currentTimeMillis();
+    String type = "technical";
+    String email = "foo@foo.com";
+    String node = "us";
+    GbrdsOrganisation go = GbrdsOrganisation.builder().name(name).primaryContactType(
+        type).primaryContactEmail(email).nodeKey(node).build();
+    CreateOrgResponse cor = registry.createOrg(go);
+    assertTrue(cor.getStatus() == HttpStatus.SC_CREATED);
+    OrgCredentials creds = cor.getResult();
+    assertNotNull(creds);
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#createResource(org.gbif.registry.api.client.GbrdsResource, org.gbif.registry.api.client.Gbrds.OrgCredentials)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#createResource(org.gbif.registry.api.client.GbrdsResource, org.gbif.registry.api.client.Gbrds.OrgCredentials)}
+   * .
    */
   @Test
   public final void testCreateResource() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#createService(org.gbif.registry.api.client.GbrdsService, org.gbif.registry.api.client.Gbrds.OrgCredentials)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#createService(org.gbif.registry.api.client.GbrdsService, org.gbif.registry.api.client.Gbrds.OrgCredentials)}
+   * .
    */
   @Test
   public final void testCreateService() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#deleteResource(java.lang.String, org.gbif.registry.api.client.Gbrds.OrgCredentials)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#deleteResource(java.lang.String, org.gbif.registry.api.client.Gbrds.OrgCredentials)}
+   * .
    */
   @Test
   public final void testDeleteResource() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#deleteService(java.lang.String, org.gbif.registry.api.client.Gbrds.OrgCredentials)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#deleteService(java.lang.String, org.gbif.registry.api.client.Gbrds.OrgCredentials)}
+   * .
    */
   @Test
   public final void testDeleteService() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#getCreds(java.lang.String, java.lang.String)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#getCreds(java.lang.String, java.lang.String)}
+   * .
    */
   @Test
   public final void testGetCreds() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#getMeta(org.gbif.registry.api.client.GbrdsOrganisation)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#getMeta(org.gbif.registry.api.client.GbrdsOrganisation)}
+   * .
    */
   @Test
   public final void testGetMetaGbrdsOrganisation() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#getMeta(org.gbif.registry.api.client.GbrdsResource)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#getMeta(org.gbif.registry.api.client.GbrdsResource)}
+   * .
    */
   @Test
   public final void testGetMetaGbrdsResource() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#getOrgBuilder(org.gbif.provider.model.ResourceMetadata)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#getOrgBuilder(org.gbif.provider.model.ResourceMetadata)}
+   * .
    */
   @Test
   public final void testGetOrgBuilder() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#getResourceBuilder(org.gbif.provider.model.ResourceMetadata)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#getResourceBuilder(org.gbif.provider.model.ResourceMetadata)}
+   * .
    */
   @Test
   public final void testGetResourceBuilder() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#getServiceUrl(org.gbif.provider.model.voc.ServiceType, org.gbif.provider.model.Resource)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#getServiceUrl(org.gbif.provider.model.voc.ServiceType, org.gbif.provider.model.Resource)}
+   * .
    */
   @Test
   public final void testGetServiceUrl() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#isLocalhost(java.lang.String)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#isLocalhost(java.lang.String)}
+   * .
    */
   @Test
   public final void testIsLocalhost() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#listAllExtensions()}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#listAllExtensions()}
+   * .
    */
   @Test
   public final void testListAllExtensions() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#listAllThesauri()}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#listAllThesauri()}
+   * .
    */
   @Test
   public final void testListAllThesauri() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#listServices(java.lang.String)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#listServices(java.lang.String)}
+   * .
    */
   @Test
   public final void testListServices() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#orgExists(java.lang.String)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#orgExists(java.lang.String)}
+   * .
    */
   @Test
   public final void testOrgExists() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#readGbrdsResource(java.lang.String)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#readGbrdsResource(java.lang.String)}
+   * .
    */
   @Test
   public final void testReadGbrdsResource() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#readOrg(java.lang.String)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#readOrg(java.lang.String)}
+   * .
    */
   @Test
   public final void testReadOrg() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#resourceExists(java.lang.String)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#resourceExists(java.lang.String)}
+   * .
    */
   @Test
   public final void testResourceExists() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#updateIptRssServiceUrl(java.lang.String, java.lang.String, java.lang.String)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#updateIptRssServiceUrl(java.lang.String, java.lang.String, java.lang.String)}
+   * .
    */
   @Test
   public final void testUpdateIptRssServiceUrl() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#updateOrg(org.gbif.registry.api.client.GbrdsOrganisation, org.gbif.registry.api.client.Gbrds.OrgCredentials)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#updateOrg(org.gbif.registry.api.client.GbrdsOrganisation, org.gbif.registry.api.client.Gbrds.OrgCredentials)}
+   * .
    */
   @Test
   public final void testUpdateOrg() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#updateResource(org.gbif.registry.api.client.GbrdsResource, org.gbif.registry.api.client.Gbrds.OrgCredentials)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#updateResource(org.gbif.registry.api.client.GbrdsResource, org.gbif.registry.api.client.Gbrds.OrgCredentials)}
+   * .
    */
   @Test
   public final void testUpdateResource() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#updateService(org.gbif.registry.api.client.GbrdsService, org.gbif.registry.api.client.Gbrds.OrgCredentials)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#updateService(org.gbif.registry.api.client.GbrdsService, org.gbif.registry.api.client.Gbrds.OrgCredentials)}
+   * .
    */
   @Test
   public final void testUpdateService() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#updateServiceUrls(java.util.List)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#updateServiceUrls(java.util.List)}
+   * .
    */
   @Test
   public final void testUpdateServiceUrls() {
-    fail("Not yet implemented"); // TODO
+
   }
 
   /**
-   * Test method for {@link org.gbif.provider.service.impl.RegistryManagerImpl#validateCreds(org.gbif.registry.api.client.Gbrds.OrgCredentials)}.
+   * Test method for
+   * {@link org.gbif.provider.service.impl.RegistryManagerImpl#validateCreds(org.gbif.registry.api.client.Gbrds.OrgCredentials)}
+   * .
    */
   @Test
   public final void testValidateCreds() {
-    fail("Not yet implemented"); // TODO
+
   }
 
 }
