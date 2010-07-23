7405a5320ecb2d3fdda3f823ed9bf798fabe1b33
josecuadra <josecuadra@660068cc-ad4d-0410-81c7-436461b1c8ff>
2010-07-23 15:54:03 +0000

    - Associate organisations to an IPT (persists already) - modified the code to fit it into the expected workflow - misc

--- trunk/gbif-ipt/src/main/java/org/gbif/ipt/action/admin/OrganisationsAction.java
+++ trunk/gbif-ipt/src/main/java/org/gbif/ipt/action/admin/OrganisationsAction.java
@@ -3,24 +3,107 @@
  */
 package org.gbif.ipt.action.admin;
 
-import org.gbif.ipt.action.BaseAction;
+import org.gbif.ipt.action.POSTAction;
+import org.gbif.ipt.model.registration.Organisation;
+import org.gbif.ipt.service.AlreadyExistingException;
+import org.gbif.ipt.service.admin.GBIFRegistryManager;
+import org.gbif.ipt.service.admin.OrganisationManager;
 
 import com.google.inject.Inject;
 
+import java.io.IOException;
+import java.util.List;
+
 /**
  * The Action responsible for all user input relating to the organisations allowed in the IPT
+ * 
  * @author tim
  */
-public class OrganisationsAction extends BaseAction {
-	private static final long serialVersionUID = 7297470324204084809L;
+public class OrganisationsAction extends POSTAction {
+  private static final long serialVersionUID = 7297470324204084809L;
 
-	// the managers for the action
-	// managers are injected by Google Guice
-	
-	// these are transient properties that are set on a per request basis
-	// getters and setters are called by the Struts2 interceptors based on the 
-	// http request submitted
-	
-	// Getters / Setters follow
-	// Managers are marked for injection by Google Guice
+  @Inject
+  private GBIFRegistryManager registryManager;
+  @Inject
+  private OrganisationManager organisationManager;
+
+  private List<Organisation> organisations;
+  private Organisation organisation;
+
+  /**
+   * @return the organisation
+   */
+  public Organisation getOrganisation() {
+    return organisation;
+  }
+
+  /**
+   * @return the organisations
+   */
+  public List<Organisation> getOrganisations() {
+    return organisations;
+  }
+
+  @Override
+  public void prepare() throws Exception {
+    super.prepare();
+    log.debug("getting list of organisations");
+    organisations = registryManager.listAllOrganisations();
+    log.debug("organisations returned: " + organisations.size());
+  }
+
+  @Override
+  public String save() {
+    System.out.println("PRINTING SAVE() KEY- " + organisation.getKey());
+    System.out.println("PRINTING SAVE() PWD- " + organisation.getPassword());
+
+    try {
+      if (organisation.getKey() != null && organisation.getPassword() != null) {
+        boolean validateStatus = registryManager.validateOrganisation(organisation.getKey(), organisation.getPassword());
+        if (validateStatus) {
+          addActionMessage("The organisation has been associated to this IPT");
+          organisationManager.add(organisation);
+          organisationManager.save();
+          return INPUT;
+        } else {
+          organisations = registryManager.listAllOrganisations();
+          addActionError("The password provided does not match the organisation's password. Association has not been made.");
+          return INPUT;
+        }
+      } else {
+        organisations = registryManager.listAllOrganisations();
+        if (organisation.getKey() == null) {
+          addFieldError("organisationKey", "Please select an organisation from the list");
+        }
+        if (organisation.getPassword() == null) {
+          addFieldError("organisationPassword", "Please fill in the password field");
+        }
+        addActionError("One or more fields are missing values");
+        return INPUT;
+      }
+    } catch (IOException e) {
+      log.error("The organisation association couldnt be saved: " + e.getMessage(), e);
+      addActionError(getText("admin.organisation.saveError"));
+      addActionError(e.getMessage());
+      return INPUT;
+    } catch (AlreadyExistingException e) {
+      addActionError(getText("admin.organisation.exists", new String[]{id,}));
+      return INPUT;
+    }
+  }
+
+  /**
+   * @param organisation the organisation to set
+   */
+  public void setOrganisation(Organisation organisation) {
+    this.organisation = organisation;
+  }
+
+  /**
+   * @param organisations the organisations to set
+   */
+  public void setOrganisations(List<Organisation> organisations) {
+    this.organisations = organisations;
+  }
+
 }
--- trunk/gbif-ipt/src/main/java/org/gbif/ipt/service/admin/impl/OrganisationManagerImpl.java
+++ trunk/gbif-ipt/src/main/java/org/gbif/ipt/service/admin/impl/OrganisationManagerImpl.java
@@ -3,14 +3,66 @@
  */
 package org.gbif.ipt.service.admin.impl;
 
+import org.gbif.ipt.model.registration.Organisation;
 import org.gbif.ipt.service.BaseManager;
 import org.gbif.ipt.service.admin.OrganisationManager;
+import org.gbif.ipt.utils.FileUtils;
 
 import com.google.inject.Singleton;
+import com.thoughtworks.xstream.XStream;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Writer;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 
 /**
  * @author tim
+ * @author josecuadra
  */
 @Singleton
 public class OrganisationManagerImpl extends BaseManager implements OrganisationManager {
+  public static final String PERSISTENCE_FILE = "organisations.xml";
+  private SortedMap<String, Organisation> organisations = new TreeMap<String, Organisation>();
+  private final XStream xstream = new XStream();
+
+  public OrganisationManagerImpl() {
+    super();
+    defineXstreamMapping();
+  }
+
+  public void add(Organisation organisation) {
+    if (organisation != null) {
+      addOrganisation(organisation);
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.gbif.ipt.service.admin.OrganisationManager#save()
+   */
+  public void save() throws IOException {
+    log.debug("Saving all user organisations associated to this IPT...");
+    Writer organisationWriter = FileUtils.startNewUtf8File(dataDir.configFile(PERSISTENCE_FILE));
+    ObjectOutputStream out = xstream.createObjectOutputStream(organisationWriter, "organisations");
+    for (Entry<String, Organisation> entry : organisations.entrySet()) {
+      out.writeObject(entry.getValue());
+    }
+    out.close();
+  }
+
+  private Organisation addOrganisation(Organisation organisation) {
+    if (organisation != null) {
+      log.debug("Adding organisation " + organisation.getKey() + " - " + organisation.getName());
+      organisations.put(organisation.getKey(), organisation);
+    }
+    return organisation;
+  }
+
+  private void defineXstreamMapping() {
+    xstream.alias("organisation", Organisation.class);
+  }
 }
