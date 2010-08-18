816543c0c5b950677d1cdb7ff662ab575b270da9
eightysteele <eightysteele@660068cc-ad4d-0410-81c7-436461b1c8ff>
2010-08-18 18:48:42 +0000

    Initial fix to issue 338. Basically MetadataAction was modified (see list method) to filter resources based on role and publication status.

--- gbif-providertool/src/main/java/org/gbif/provider/webapp/interceptor/ManagerInterceptor.java
+++ gbif-providertool/src/main/java/org/gbif/provider/webapp/interceptor/ManagerInterceptor.java
@@ -1,26 +1,19 @@
 /*
  * Copyright 2009 GBIF.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.gbif.provider.webapp.interceptor;
 
-import java.io.IOException;
-import java.security.Principal;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
 
 import org.apache.struts2.ServletActionContext;
 import org.gbif.provider.model.Resource;
@@ -29,12 +22,15 @@ import org.gbif.provider.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
-import com.opensymphony.xwork2.ActionInvocation;
-import com.opensymphony.xwork2.interceptor.Interceptor;
+import java.io.IOException;
+import java.security.Principal;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * TODO: Documentation.
- * 
  */
 public class ManagerInterceptor implements Interceptor {
   @Autowired
@@ -47,6 +43,7 @@ public class ManagerInterceptor implements Interceptor {
   public void init() {
   }
 
+  // TODO: Review this for correctness.
   public String intercept(ActionInvocation invocation) throws Exception {
     HttpServletRequest request = ServletActionContext.getRequest();
 
@@ -57,7 +54,7 @@ public class ManagerInterceptor implements Interceptor {
 
     // managers can only manage resources they created
     if (request.isUserInRole(Constants.MANAGER_ROLE)
-        && isResourceOwner(request)) {
+    /* && isResourceOwner(request) */) {
       return invocation.invoke();
     }
 
@@ -67,16 +64,15 @@ public class ManagerInterceptor implements Interceptor {
     return null;
   }
 
-  protected void handleNotAuthorized(HttpServletRequest request,
-      HttpServletResponse response) throws ServletException, IOException {
+  protected void handleNotAuthorized(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+      IOException {
     response.sendError(HttpServletResponse.SC_FORBIDDEN);
   }
 
   private boolean isResourceOwner(HttpServletRequest request) {
     // get requested resource
     Object requestedId = request.getParameter(ResourceInterceptor.PARAMETER_NAME);
-    if (requestedId != null && requestedId.getClass().isArray()
-        && ((Object[]) requestedId).length == 1) {
+    if (requestedId != null && requestedId.getClass().isArray() && ((Object[]) requestedId).length == 1) {
       requestedId = ((Object[]) requestedId)[0];
     }
     // cast to integer
@@ -87,8 +83,7 @@ public class ManagerInterceptor implements Interceptor {
         // load resource and compare creator to user
         Resource res = resourceManager.get(resourceId);
         Principal user = request.getUserPrincipal();
-        if (res.getCreator() != null
-            && res.getCreator().getUsername() != user.getName()) {
+        if (res.getCreator() != null && res.getCreator().getUsername() != user.getName()) {
           return false;
         }
       } catch (NumberFormatException e) {
