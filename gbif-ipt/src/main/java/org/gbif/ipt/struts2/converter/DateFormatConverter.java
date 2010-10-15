5fdb70b48ac681e51d04ab2b3113959b3d6c96c2
htobon@gmail.com <htobon@gmail.com@660068cc-ad4d-0410-81c7-436461b1c8ff>
2010-10-15 13:36:55 +0000

    The user can now add a date only writing a year.

--- gbif-ipt/src/main/java/org/gbif/ipt/struts2/converter/DateFormatConverter.java
+++ gbif-ipt/src/main/java/org/gbif/ipt/struts2/converter/DateFormatConverter.java
@@ -17,16 +17,27 @@ public class DateFormatConverter extends StrutsTypeConverter {
 		if (values != null && values.length > 0) {
 			Date date = null;
 			try {
-				// evaluating 
+				if (values[0].length() == 4) {
+					// A date containing only a year is going to be represented as a date with 1 millisecond (yyyy-01/01 00:00:00:01). 
+					// This is an internal protocol implemented to communicate with the free marker. See EmlFactory#calendarDate.
+					SimpleDateFormat usaFormat = new SimpleDateFormat("yyyy");
+					date = usaFormat.parse(values[0]);	
+					date = new Date(date.getTime()+1);
+				}
+			} catch (ParseException e) {
+			}
+			try {
 				SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
 				date = isoFormat.parse(values[0]);
-			
+			} catch (ParseException e1) {
+			}
+			try {
 				SimpleDateFormat usaFormat = new SimpleDateFormat("dd/MM/yyyy");
 				date = usaFormat.parse(values[0]);
 			} catch (ParseException e) {
-				
 			}
-			// TODO - There should be a way to send an error to the user telling that the date has a wrong format. 
+			
+			// TODO - There should be a way to send an error to the user telling that the date has a wrong format.
 			// Throwing XWorkException or TypeConversionException doesn't work.
 			// 
 			return date;
@@ -40,7 +51,7 @@ public class DateFormatConverter extends StrutsTypeConverter {
 			Date d = (Date) o;
 			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 			return format.format(d);
-		} 
+		}
 		return null;
 	}
 
