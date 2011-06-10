953542d47b76f19bad5ccd4eeac6c62def5c42cd
htobon <htobon@660068cc-ad4d-0410-81c7-436461b1c8ff>
2011-06-10 21:00:32 +0000

    JUnit Test for Eml2Rtf.

--- gbif-ipt/src/test/java/org/gbif/ipt/task/Eml2RtfTest.java
+++ gbif-ipt/src/test/java/org/gbif/ipt/task/Eml2RtfTest.java
@@ -13,17 +13,9 @@
 
 package org.gbif.ipt.task;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Locale;
-
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.gbif.ipt.action.BaseAction;
+import org.gbif.ipt.config.AppConfig;
+import org.gbif.ipt.mock.MockAppConfig;
 import org.gbif.ipt.mock.MockVocabulariesManager;
 import org.gbif.ipt.model.Resource;
 import org.gbif.ipt.model.User;
@@ -31,17 +23,32 @@ import org.gbif.ipt.service.admin.VocabulariesManager;
 import org.gbif.ipt.struts2.SimpleTextProvider;
 import org.gbif.ipt.utils.IptMockBaseTest;
 import org.gbif.metadata.eml.EmlFactory;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.xml.sax.SAXException;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.rtf.RtfWriter2;
+import com.opensymphony.xwork2.LocaleProvider;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * @author htobon
@@ -49,12 +56,12 @@ import com.lowagie.text.rtf.RtfWriter2;
 @RunWith(MockitoJUnitRunner.class)
 public class Eml2RtfTest extends IptMockBaseTest {
 
-  private Locale defaultLocale = new Locale("en");
   private VocabulariesManager mockedVocabManager;
-  private SimpleTextProvider textProvider;
   private Eml2Rtf eml2Rtf;
+  private BaseAction action;
+  private AppConfig cfg;
+  private SimpleTextProvider textProvider;
 
-  @Ignore
   @Test
   public void generateRtfFile() {
     File rtfTempFile = null;
@@ -62,23 +69,23 @@ public class Eml2RtfTest extends IptMockBaseTest {
       Document doc = new Document(PageSize.LETTER);
       Resource resource = new Resource();
       resource.setEml(EmlFactory.build(new FileInputStream("./src/test/resources/data/eml.xml"))); // or eml2.xml
-      // resource.setEml(EmlFactory.build(new FileInputStream("D:\\GBIF\\IPT\\resources\\indfauna\\eml.xml"))); // or
-// eml2.xml
       resource.setShortname("resource");
       User creator = new User();
       creator.setFirstname("Markus");
-      creator.setLastname("D�ring");
+      creator.setLastname("Doring");
       resource.setCreator(creator);
       rtfTempFile = File.createTempFile("resource", ".rtf");
       System.out.println("Writing temporary test RTF file to " + rtfTempFile.getAbsolutePath());
-
       OutputStream out = new FileOutputStream(rtfTempFile);
-
       RtfWriter2.getInstance(doc, out);
-      eml2Rtf.writeEmlIntoRtf(doc, resource, new BaseAction());
+      eml2Rtf.writeEmlIntoRtf(doc, resource, action);
       out.close();
-      // Runtime.getRuntime().exec("C:/Program Files/Microsoft Office/Office12/WINWORD.EXE " +
-// rtfTempFile.getAbsolutePath());
+      /*
+       * FOR WINDOWS XP TEST ONLY
+       * Runtime.getRuntime().exec(
+       * "C:/Program Files/Microsoft Office/Office12/WINWORD.EXE " + rtfTempFile.getAbsolutePath());
+       */
+
       // Do not comment the following line if you are going to commit this code.
       rtfTempFile.deleteOnExit();
     } catch (FileNotFoundException e) {
@@ -94,7 +101,7 @@ public class Eml2RtfTest extends IptMockBaseTest {
   }
 
   /**
-   * This method should be used to configure some Mock class. If needed.
+   * This method should be used to configure some Mock class if needed.
    * 
    * @throws SAXException
    * @throws ParserConfigurationException
@@ -103,9 +110,17 @@ public class Eml2RtfTest extends IptMockBaseTest {
   public void setUp() throws ParserConfigurationException, SAXException {
     eml2Rtf = new Eml2Rtf();
     mockedVocabManager = new MockVocabulariesManager();
-    textProvider = new SimpleTextProvider();
-    textProvider.setDefaultLocale(defaultLocale.toString());
     eml2Rtf.setTextProvider(textProvider);
     eml2Rtf.setVocabManager(mockedVocabManager);
+
+    // Building mocks.
+    cfg = MockAppConfig.buildMock();
+    textProvider = mock(SimpleTextProvider.class);
+
+    // Stubbing TextProvider
+    when(textProvider.getText(any(LocaleProvider.class), anyString(), anyString(), any(Object[].class))).thenReturn(
+        "i18n-word");
+
+    action = new BaseAction(textProvider, cfg);
   }
 }
