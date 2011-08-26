969e33a92da32490f20ac82c0480360e3eb3d466
jlthlopez <jlthlopez@660068cc-ad4d-0410-81c7-436461b1c8ff>
2011-08-26 21:45:34 +0000

    Validating the decimal field values in the Geographic coverage page.

---
+++ gbif-ipt/src/main/java/org/gbif/ipt/struts2/converter/LatitudeFormatConverter.java
@@ -0,0 +1,50 @@
+/***************************************************************************
+ * Copyright 2011 Global Biodiversity Information Facility Secretariat
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ***************************************************************************/
+
+package org.gbif.ipt.struts2.converter;
+
+import java.util.Map;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.lang.xwork.math.DoubleRange;
+import org.apache.struts2.util.StrutsTypeConverter;
+
+/**
+ * This class validate if the longitude field value is a decimal number.
+ * 
+ * @author julieth
+ */
+public class LatitudeFormatConverter extends StrutsTypeConverter {
+
+  @Override
+  public Object convertFromString(Map context, String[] values, Class toClass) {
+    // The latitude is validating in a range of doubles
+    DoubleRange range = new DoubleRange(-90, 90);
+    try {
+      if (range.containsDouble(Double.parseDouble(values[0]))) {
+        return NumberUtils.toDouble(values[0]);
+      }
+    } catch (NumberFormatException e) {
+    }
+    return null;
+  }
+
+  @Override
+  public String convertToString(Map context, Object o) {
+    if (o instanceof Double) {
+      return ((Double) o).toString();
+    }
+    return null;
+  }
+
+}
