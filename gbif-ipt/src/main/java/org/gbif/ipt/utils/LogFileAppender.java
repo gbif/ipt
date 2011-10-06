4576a7fa39bf2a53917acc1a709e5139b813dfe5
htobon <htobon@660068cc-ad4d-0410-81c7-436461b1c8ff>
2011-10-06 14:58:28 +0000

    Configuring temporal path to locate the logs files while the IPT adminsitrator configures a properly DataDir.

--- gbif-ipt/src/main/java/org/gbif/ipt/utils/LogFileAppender.java
+++ gbif-ipt/src/main/java/org/gbif/ipt/utils/LogFileAppender.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2009 GBIF. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
@@ -13,33 +13,55 @@ import java.io.IOException;
 import org.apache.log4j.RollingFileAppender;
 
 /**
- * TODO: Documentation.
+ * LogFileAppender works to backup the IPT log files (admin.log, debug.log). Plus, it looks for a temporal path location
+ * while Tomcat, Jetty, or any other server initialise the project at first time before the user is allowed to configure
+ * a properly DataDir.
  */
 public class LogFileAppender extends RollingFileAppender {
 
   public static String LOGDIR = "";
- public static String[] DIRECTORIES = new String[] {"", System.getProperty("user.home"),
- System.getProperty("user.dir"), System.getProperty("java.io.tmpdir")};
-  
+  // temporal paths which can be used depending on the OS.
+  private static final String[] PATHS = new String[] {System.getProperty("java.io.tmpdir"),
+    System.getProperty("user.home"), System.getProperty("user.dir")};
+
+  /**
+   * Find temporal path with writing permissions depending on the Operating System.
+   * 
+   * @return the location of the temporal file.
+   */
+  private String findTempDir() {
+    File logFile = null;
+    for (String path : PATHS) {
+      // Create file instance.
+      logFile = new File(path, "admin.log");
+
+      // Has the file writing permissions?
+      try {
+        logFile.createNewFile();
+      } catch (IOException e) {
+        // Do nothing here.
+      }
+      if (logFile.canWrite()) {
+        return path;
+      }
+    }
+    return "";
+  }
+
   @Override
   public synchronized void setFile(String fileName, boolean append, boolean bufferedIO, int bufferSize)
     throws IOException {
-    File logFile = null;
-    for (String dir : DIRECTORIES) {
-      // Creating a File instance.
-      logFile = new File(dir, fileName);
-      
-      // Has the File writing permissions?
-      logFile.createNewFile();
-      if (logFile.canWrite()) {
-        break;
-      }
-    }
+    File logfile = new File(fileName);
 
-    if (logFile != null) {
-      fileName = logFile.getAbsolutePath();
+    if (!LOGDIR.equals("")) {
+      // modify fileName if relative
+      if (!logfile.isAbsolute()) {
+        fileName = LOGDIR + File.separator + fileName;
+      }
+    } else {
+      // if LOGDIR is not initialised, find a temporal location while user configure the IPT DataDir.
+      fileName = findTempDir() + File.separator + fileName;
     }
     super.setFile(fileName, append, bufferedIO, bufferSize);
   }
 }
-
