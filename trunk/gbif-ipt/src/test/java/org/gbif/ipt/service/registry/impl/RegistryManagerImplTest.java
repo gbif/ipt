77c97924549b60635900b83cd9377f4ac47f63ed
kyle.braak@gmail.com <kyle.braak@gmail.com@660068cc-ad4d-0410-81c7-436461b1c8ff>
2015-02-10 16:16:02 +0000

    Change ensures param "doi" is included in register resource and update resource calls (when latest published version has been assigned a DOI). Change also separates out ITs from RegistryManagerImplTest - those tests hitting sandbox registry (legacy) web services.
    Update Issue 1131

--- trunk/gbif-ipt/src/test/java/org/gbif/ipt/service/registry/impl/RegistryManagerImplTest.java
+++ trunk/gbif-ipt/src/test/java/org/gbif/ipt/service/registry/impl/RegistryManagerImplTest.java
@@ -17,25 +17,20 @@ import org.gbif.ipt.config.AppConfig;
 import org.gbif.ipt.config.ConfigWarnings;
 import org.gbif.ipt.config.DataDir;
 import org.gbif.ipt.model.Extension;
-import org.gbif.ipt.model.Ipt;
 import org.gbif.ipt.model.Organisation;
 import org.gbif.ipt.model.Resource;
-import org.gbif.ipt.model.User;
 import org.gbif.ipt.model.Vocabulary;
 import org.gbif.ipt.service.RegistryException;
 import org.gbif.ipt.service.admin.RegistrationManager;
 import org.gbif.ipt.service.registry.RegistryManager;
 import org.gbif.ipt.struts2.SimpleTextProvider;
 import org.gbif.ipt.utils.IptMockBaseTest;
-import org.gbif.metadata.eml.Eml;
 import org.gbif.utils.HttpUtil;
 
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.URISyntaxException;
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 
@@ -46,10 +41,7 @@ import org.junit.Test;
 import org.xml.sax.SAXException;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -58,12 +50,6 @@ public class RegistryManagerImplTest extends IptMockBaseTest {
 
   // logging
   private static final Logger LOG = Logger.getLogger(RegistryManagerImplTest.class);
-
-  private static final String ORG_UUID = "62922b92-69d1-4c4b-831c-b23d5412a124";
-  private static final String ORG_PASSWORD = "password";
-  private static final String ORG_NAME = "AAA4Organisation";
-
-  private RegistryManager manager;
 
   private AppConfig mockAppConfig;
   private DataDir mockDataDir;
@@ -74,87 +60,16 @@ public class RegistryManagerImplTest extends IptMockBaseTest {
   private HttpUtil mockHttpUtil;
   private HttpUtil.Response mockResponse;
 
-  private Organisation organisation;
-  private Ipt ipt;
-
   @Before
   public void setup() throws SAXException, ParserConfigurationException {
     mockHttpUtil = mock(HttpUtil.class);
     mockResponse = mock(HttpUtil.Response.class);
-
     mockAppConfig = mock(AppConfig.class);
     mockDataDir = mock(DataDir.class);
     mockSAXParserFactory = mock(SAXParserFactory.class);
     mockConfigWarnings = mock(ConfigWarnings.class);
     mockSimpleTextProvider = mock(SimpleTextProvider.class);
     mockRegistrationManager = mock(RegistrationManager.class);
-
-    organisation = new Organisation();
-    organisation.setKey(ORG_UUID);
-    organisation.setName(ORG_NAME);
-    organisation.setPassword(ORG_PASSWORD);
-
-    ipt = new Ipt();
-
-    // manager that issues real http requests
-    manager =
-      new RegistryManagerImpl(cfg, dataDir, new HttpUtil(buildHttpClient()), buildSaxFactory(), mockConfigWarnings,
-        mockSimpleTextProvider, mockRegistrationManager);
-  }
-
-  @Test
-  public void testBuild() {
-    try {
-      // test organisation "AAA4Organisation"
-      assertTrue(manager.validateOrganisation(ORG_UUID, ORG_PASSWORD));
-      assertFalse(manager.validateOrganisation(ORG_UUID, ORG_NAME));
-      assertFalse(manager.validateOrganisation("INVALID92-69d1-4c4b-831c-b23d5412a124", ORG_PASSWORD));
-
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail(e.getMessage());
-    }
-  }
-
-  @Test
-  public void testWriteReadResource() {
-    try {
-      ipt.setCreated(new Date());
-      ipt.setDescription("a unit test mock IPT");
-      ipt.setPrimaryContactName("Ms. IPT Admin");
-      ipt.setPrimaryContactEmail("gbif@mailinator.com");
-      ipt.setPrimaryContactType("technical");
-      ipt.setLanguage("en");
-      ipt.setName("Mock IPT");
-
-      // register IPT
-      ipt.setKey(manager.registerIPT(ipt, organisation));
-
-      // register resource
-      Resource res = new Resource();
-      User user = new User();
-      user.setFirstname("Mock Name");
-      user.setEmail("mocking@themock.org");
-      res.setCreator(user);
-      res.setShortname("mock");
-      res.setTitle("Möck rèşürçe wíŧħ ƒũñňÿ ćĥåřæċŧëŗş");
-      res.setCreated(new Date());
-      res.setSubtype("occurrence test");
-      Eml eml = new Eml();
-      eml.setAbstract(
-        "An IPT unit test resource that can be deleted. Testing unicode characters like ą ć ę ł ń  ś ź ż (for polish) ť ů ž č ď ě ň ř š (for czech) and other taken from http://www.alanflavell.org.uk/unicode/unidata.html  ᠀᠔ᡎᢥ(mongolian) ⅛  Ⅳ ⅸ ↂ (numbers) ∀ ∰ ⊇ ⋩ (maths) CJK Symbols and Punctuation U+3000 – U+303F (12288–12351) 々 〒 〣 〰 Hiragana U+3040 – U+309F (12352–12447) あ ぐ る ゞ Katakana U+30A0 – U+30FF (12448–12543) ア ヅ ヨ ヾ Bopomofo U+3100 – U+312F (12544–12591) ㄆ ㄓ ㄝ ㄩ Hangul Compatibility Jamo U+3130 – U+318F (12592–12687) ㄱ ㄸ ㅪ ㆍ Kanbun U+3190 – U+319F (12688–12703) ㆐ ㆕ ㆚ ㆟ Bopomofo Extended U+31A0 – U+32BF (12704–12735) ㆠ ㆧ ㆯ ㆷ Katakana Phonetic Extensions U+31F0 – U+31FF (12784–12799) ㇰ ㇵ ㇺ ㇿ Enclosed CJK Letters and Months U+3200 – U+32FF (12800–13055) ㈔ ㈲ ㊧ ㋮ CJK Compatibility U+3300 – U+33FF (13056–13311) ㌃ ㍻ ㎡ ㏵ CJK Unified Ideographs Extension A U+3400 – U+4DB5 (13312–19893) 㐅 㒅 㝬 㿜 Yijing Hexagram Symbols U+4DC0 – U+4DFF (19904–19967) ䷂ ䷫ ䷴ ䷾ CJK Unified Ideographs U+4E00 – U+9FFF (19968–40959) 一 憨 田 龥 Yi Syllables U+A000 – U+A48F (40960–42127) ꀀ ꅴ ꊩ ꒌ Yi Radicals U+A490 – U+A4CF (42128–42191) ꒐ ꒡ ꒰ ꓆ ");
-      res.setEml(eml);
-
-      res.setLastPublished(new Date());
-      UUID uuid = manager.register(res, organisation, ipt);
-      assertNotNull(uuid);
-
-      // get resource and compare
-      // TODO: no registry method for this yet
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail(e.getMessage());
-    }
   }
 
   @Test
@@ -237,10 +152,9 @@ public class RegistryManagerImplTest extends IptMockBaseTest {
   @Test
   public void testGetVocabularies() throws SAXException, ParserConfigurationException, IOException, URISyntaxException {
     // mock response from Registry with local test resource
-    String response =
+    mockResponse.content =
       IOUtils.toString(RegistryManagerImplTest.class.getResourceAsStream("/responses/thesauri.json"), "UTF-8");
 
-    mockResponse.content = response;
     when(mockHttpUtil.get(anyString())).thenReturn(mockResponse);
 
     // create instance of RegistryManager
@@ -256,10 +170,9 @@ public class RegistryManagerImplTest extends IptMockBaseTest {
   public void testGetOrganisationsResources()
     throws IOException, URISyntaxException, SAXException, ParserConfigurationException {
     // mock response from Registry with local test resource
-    String response =
-      IOUtils.toString(RegistryManagerImplTest.class.getResourceAsStream("/responses/organisations_resources.json"), "UTF-8");
+    mockResponse.content = IOUtils
+      .toString(RegistryManagerImplTest.class.getResourceAsStream("/responses/organisations_resources.json"), "UTF-8");
 
-    mockResponse.content = response;
     when(mockHttpUtil.get(anyString())).thenReturn(mockResponse);
 
     // create instance of RegistryManager
@@ -272,13 +185,11 @@ public class RegistryManagerImplTest extends IptMockBaseTest {
   }
 
   @Test
-  public void testGetOrganisation()
-    throws IOException, URISyntaxException, SAXException, ParserConfigurationException {
+  public void testGetOrganisation() throws IOException, URISyntaxException, SAXException, ParserConfigurationException {
     // mock response from Registry with local test resource
-    String response =
+    mockResponse.content =
       IOUtils.toString(RegistryManagerImplTest.class.getResourceAsStream("/responses/organisation.json"), "UTF-8");
 
-    mockResponse.content = response;
     when(mockHttpUtil.get(anyString())).thenReturn(mockResponse);
 
     // create instance of RegistryManager
