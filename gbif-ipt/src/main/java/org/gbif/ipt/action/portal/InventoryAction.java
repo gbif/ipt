a0a37dfa00d452b4cf82f9d5458fd81d6b0edef3
kyle.braak@gmail.com <kyle.braak@gmail.com@660068cc-ad4d-0410-81c7-436461b1c8ff>
2015-02-18 11:57:03 +0000

    Fixes invalid JSON in inventory, and includes list of extensions also
    Update Issue 1142

--- gbif-ipt/src/main/java/org/gbif/ipt/action/portal/InventoryAction.java
+++ gbif-ipt/src/main/java/org/gbif/ipt/action/portal/InventoryAction.java
@@ -2,16 +2,16 @@ package org.gbif.ipt.action.portal;
 
 
 import org.gbif.ipt.config.AppConfig;
+import org.gbif.ipt.model.Extension;
 import org.gbif.ipt.model.Resource;
 import org.gbif.ipt.model.voc.PublicationStatus;
 import org.gbif.ipt.service.manage.ResourceManager;
 
-import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import com.google.common.base.Strings;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.json.annotations.JSON;
@@ -21,19 +21,9 @@ import org.apache.struts2.json.annotations.JSON;
  */
 public class InventoryAction extends ActionSupport {
 
-  // format used to format/parse a Date field
-  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-  private static final String TITLE_KEY = "title";
-  private static final String TYPE_KEY = "type";
-  private static final String COUNT_KEY = "records";
-  private static final String LAST_PUBLISHED_KEY = "lastPublished";
-  private static final String GBIF_UUID_KEY = "gbifKey";
-  private static final String EML_KEY = "eml";
-  private static final String DWCA_KEY = "dwca";
-
   private final AppConfig cfg;
   private final ResourceManager resourceManager;
-  private List<Resource> registered;
+  private List<DatasetItem> inventory;
 
   @Inject
   public InventoryAction(AppConfig cfg, ResourceManager resourceManager) {
@@ -42,29 +32,154 @@ public class InventoryAction extends ActionSupport {
   }
 
   public String execute() {
-    // load all registered resources
-    registered = resourceManager.list(PublicationStatus.REGISTERED);
+    // load all registered resources, and populate inventory
+    List<Resource> registered = resourceManager.list(PublicationStatus.REGISTERED);
+    if (!registered.isEmpty()) {
+      populateInventory(registered);
+    }
     return SUCCESS;
   }
 
-  /**
-   * @return JSON array as string representing all registered resources, null values converted to empty string
-   */
   @JSON(name = "registeredResources")
-  public String getResources() {
-    JsonArray jsonArray = new JsonArray();
-    // convert each registered resource into JsonObject with select properties
-    for (Resource r : registered) {
-      JsonObject jsonObject = new JsonObject();
-      jsonObject.addProperty(TITLE_KEY, Strings.nullToEmpty(r.getTitle()));
-      jsonObject.addProperty(TYPE_KEY, Strings.nullToEmpty(r.getCoreType()));
-      jsonObject.addProperty(COUNT_KEY, String.valueOf(r.getRecordsPublished()));
-      jsonObject.addProperty(LAST_PUBLISHED_KEY, Strings.nullToEmpty(DATE_FORMAT.format(r.getLastPublished())));
-      jsonObject.addProperty(GBIF_UUID_KEY, (r.getKey() == null) ? "" : r.getKey().toString());
-      jsonObject.addProperty(EML_KEY, cfg.getResourceEmlUrl(r.getShortname()));
-      jsonObject.addProperty(DWCA_KEY, cfg.getResourceArchiveUrl(r.getShortname()));
-      jsonArray.add(jsonObject);
+  public List<DatasetItem> getInventory() {
+    return inventory;
+  }
+
+  public void setInventory(List<DatasetItem> inventory) {
+    this.inventory = inventory;
+  }
+
+  /**
+   * Class representing dataset item returned in inventory response serialized into JSON.
+   */
+  public class DatasetItem {
+
+    private String title;
+    private String type;
+    private int records;
+    private Date lastPublished;
+    private String gbifKey;
+    private String eml;
+    private String dwca;
+    private List<String> extensions;
+
+    /**
+     * @return the dataset title
+     */
+    public String getTitle() {
+      return title;
     }
-    return jsonArray.toString();
+
+    public void setTitle(String title) {
+      this.title = title;
+    }
+
+    /**
+     * @return the dataset type
+     */
+    public String getType() {
+      return type;
+    }
+
+    public void setType(String type) {
+      this.type = type;
+    }
+
+    /**
+     * @return the dataset record count
+     */
+    public int getRecords() {
+      return records;
+    }
+
+    public void setRecords(int records) {
+      this.records = records;
+    }
+
+    /**
+     * @return the date the dataset was last published
+     */
+    @JSON(format = "yyyy-MM-dd")
+    public Date getLastPublished() {
+      return lastPublished;
+    }
+
+    public void setLastPublished(Date lastPublished) {
+      this.lastPublished = lastPublished;
+    }
+
+    /**
+     * @return the dataset GBIF key (UUID)
+     */
+    public String getGbifKey() {
+      return gbifKey;
+    }
+
+    public void setGbifKey(String gbifKey) {
+      this.gbifKey = gbifKey;
+    }
+
+    /**
+     * @return the endpoint URL to the dataset's last published Eml file
+     */
+    public String getEml() {
+      return eml;
+    }
+
+    public void setEml(String eml) {
+      this.eml = eml;
+    }
+
+    /**
+     * @return the endpoint URL to the dataset's last published DwC-A file
+     */
+    public String getDwca() {
+      return dwca;
+    }
+
+    public void setDwca(String dwca) {
+      this.dwca = dwca;
+    }
+
+    /**
+     * @return the extension rowTypes of the extensions used in the last published DwC-A file, or an empty list
+     */
+    public List<String> getExtensions() {
+      if (extensions == null) {
+        extensions = Lists.newArrayList();
+      }
+      return extensions;
+    }
+
+    public void setExtensions(List<String> extensions) {
+      this.extensions = extensions;
+    }
+  }
+
+  /**
+   * Populate and set the list/inventory of DatasetItem. Each DatasetItem is populated from a registered Resource.
+   *
+   * @param resources registered resources list
+   */
+  public void populateInventory(List<Resource> resources) {
+    List<DatasetItem> items = Lists.newArrayList();
+    for (Resource r : resources) {
+      DatasetItem item = new DatasetItem();
+      item.setTitle(Strings.nullToEmpty(r.getTitle()));
+      item.setType(Strings.nullToEmpty(r.getCoreType()));
+      item.setRecords(r.getRecordsPublished());
+      item.setLastPublished(r.getLastPublished());
+      item.setGbifKey(r.getKey().toString());
+      item.setEml(cfg.getResourceEmlUrl(r.getShortname()));
+      item.setDwca(cfg.getResourceArchiveUrl(r.getShortname()));
+      // populate list of extension rowTypes (excluding core extensions)
+      for (Extension extension: r.getMappedExtensions()) {
+        if (!extension.isCore()) {
+          item.getExtensions().add(extension.getRowType());
+        }
+      }
+      items.add(item);
+    }
+    setInventory(items);
   }
 }
