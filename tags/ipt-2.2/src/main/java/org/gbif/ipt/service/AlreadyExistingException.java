a9f3ac26c677208889b7add394407b6705c85081
kyle.braak@gmail.com <kyle.braak@gmail.com@660068cc-ad4d-0410-81c7-436461b1c8ff>
2015-03-25 12:08:51 +0000

    Deleting tag

--- tags/ipt-2.2/src/main/java/org/gbif/ipt/service/AlreadyExistingException.java
+++
@@ -1,8 +0,0 @@
-package org.gbif.ipt.service;
-
-/**
- * Exception thrown when adding an entity with a unique ID which exists already.
- */
-public class AlreadyExistingException extends Exception {
-
-}
