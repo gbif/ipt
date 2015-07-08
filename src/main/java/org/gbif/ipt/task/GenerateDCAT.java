01a3586aa185ebd8df59f343cd7ed95813160a32
Delbauve Sylvain <theamenai@hotmail.fr>
2015-07-08 11:48:51 +0200

    Just the first test for git

---
+++ src/main/java/org/gbif/ipt/task/GenerateDCAT.java
@@ -0,0 +1,11 @@
+package org.gbif.ipt.task;
+
+/**
+ * Created by sylvain on 8/07/15.
+ */
+public class GenerateDCAT {
+
+    public static void main (String[]args){
+        System.out.println("HELLO GIT");
+    }
+}
