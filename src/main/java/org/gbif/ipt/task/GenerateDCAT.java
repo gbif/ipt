56cd0c5ccff69fc0a75b1ae021a099dc95e6c23c
Simon Van Cauter <simonvancauter@gmail.com>
2015-07-10 15:55:13 +0200

    Created a test class and started working on the DCAT dataset generation

--- src/main/java/org/gbif/ipt/task/GenerateDCAT.java
+++ src/main/java/org/gbif/ipt/task/GenerateDCAT.java
@@ -1,6 +1,7 @@
 package org.gbif.ipt.task;
 
 import org.gbif.ipt.model.Resource;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.BufferedWriter;
@@ -11,6 +12,9 @@ import java.io.PrintWriter;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+
+import org.gbif.metadata.eml.Eml;
+import org.gbif.metadata.eml.KeywordSet;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -19,19 +23,18 @@ import org.w3c.dom.NamedNodeMap;
 /**
  * DCAT FILE EXAMPLE
  *
- *@prefix dcat: <http://www.w3.org/ns/dcat#> .
- *@prefix dc: <http://purl.org/dc/terms/> .
- *@prefix foaf: <http://xmlns.com/foaf/0.1/> .
- *<http://datatank4.gent.be/bestuurenbeleid/schepenen>
- *a dcat:Dataset ;
- *dc:title "bestuurenbeleid/schepenen" ;
- *dc:description """Schepenen van Stad Gent.
- *""" ;
- *dc:identifier "bestuurenbeleid/schepenen" ;
- *dc:issued "2015-01-15T13:05:42+0000" ;
- *dc:modified "2015-01-15T13:05:42+0000" ;
- *dcat:distribution <http://datatank4.gent.be/bestuurenbeleid/schepenen.json> .
- *
+ * @prefix dcat: <http://www.w3.org/ns/dcat#> .
+ * @prefix dc: <http://purl.org/dc/terms/> .
+ * @prefix foaf: <http://xmlns.com/foaf/0.1/> .
+ * <http://datatank4.gent.be/bestuurenbeleid/schepenen>
+ * a dcat:Dataset ;
+ * dc:title "bestuurenbeleid/schepenen" ;
+ * dc:description """Schepenen van Stad Gent.
+ * """ ;
+ * dc:identifier "bestuurenbeleid/schepenen" ;
+ * dc:issued "2015-01-15T13:05:42+0000" ;
+ * dc:modified "2015-01-15T13:05:42+0000" ;
+ * dcat:distribution <http://datatank4.gent.be/bestuurenbeleid/schepenen.json> .
  */
 
 public class GenerateDCAT {
@@ -42,20 +45,20 @@ public class GenerateDCAT {
     private  Eml eml;
     private  Organisation organisation;*/
 
-    public void create(Resource resource){
+    public void create(Resource resource) {
         //resource.setShortname("Testing Resource");
-        System.out.println("-----------------------------------\n"+"         Testing Resource\n"+"-----------------------------------\n"+ resource.toString());
+        System.out.println("-----------------------------------\n" + "         Testing Resource\n" + "-----------------------------------\n" + resource.toString());
 
-        HashMap<String,String> prefix = new HashMap<String,String>();
+        HashMap<String, String> prefix = new HashMap<String, String>();
 
-        prefix.put("dct:","http://purl.org/dc/terms/");
-        prefix.put("dcat:","http://www.w3.org/ns/dcat#");
-        prefix.put("xsd:","http://www.w3.org/2001/XMLSchema#");
-        prefix.put("skos:","http://www.w3.org/2004/02/skos/core#");
-        prefix.put("rdfs:","http://www.w3.org/2000/01/rdf-schema#");
-        prefix.put("foaf:","http://xmlns.com/foaf/0.1/");
-        prefix.put("schema:","http://schema.org/");
-        prefix.put("adms:","http://www.w3.org/ns/adms#");
+        prefix.put("dct:", "http://purl.org/dc/terms/");
+        prefix.put("dcat:", "http://www.w3.org/ns/dcat#");
+        prefix.put("xsd:", "http://www.w3.org/2001/XMLSchema#");
+        prefix.put("skos:", "http://www.w3.org/2004/02/skos/core#");
+        prefix.put("rdfs:", "http://www.w3.org/2000/01/rdf-schema#");
+        prefix.put("foaf:", "http://xmlns.com/foaf/0.1/");
+        prefix.put("schema:", "http://schema.org/");
+        prefix.put("adms:", "http://www.w3.org/ns/adms#");
 
         ArrayList<String> txt = readXmlFile();
         ArrayList<String> finalTxt = hasMapToStringArray(prefix);
@@ -66,20 +69,20 @@ public class GenerateDCAT {
 
     /**
      * Convert the prefix Hashmap to a array list
+     *
      * @param prefix
      * @return
      */
-    private static ArrayList<String> hasMapToStringArray(HashMap<String,String> prefix){
-        Object [] keyArray = prefix.keySet().toArray();
+    private static ArrayList<String> hasMapToStringArray(HashMap<String, String> prefix) {
+        Object[] keyArray = prefix.keySet().toArray();
         String[] strArray = new String[keyArray.length];
-        for (int i = 0; i < keyArray.length; i++)
-        {
+        for (int i = 0; i < keyArray.length; i++) {
             strArray[i] = new String((String) keyArray[i]);
         }
         ArrayList<String> text = new ArrayList<String>();
-        for (String key : strArray){
+        for (String key : strArray) {
             String link = prefix.get(key);
-            String line = "@prefix "+key+link;
+            String line = "@prefix " + key + link;
             text.add(line);
         }
         return text;
@@ -87,47 +90,46 @@ public class GenerateDCAT {
 
     /**
      * Write a new file with information in it
-     *  @param informations
+     *
+     * @param informations
      */
-    private static void writeFile(ArrayList<String> informations ) {
-        File fnew = new File ("/home/sylvain/Documents/datadir/dcat.txt");//Need to be fix HARDCODING
-        try
-        {
-            PrintWriter pwnew = new PrintWriter (new BufferedWriter (new FileWriter (fnew)));
-            for(String info: informations){
+    private static void writeFile(ArrayList<String> informations) {
+        File fnew = new File("/home/sylvain/Documents/datadir/dcat.txt");//Need to be fix HARDCODING
+        try {
+            PrintWriter pwnew = new PrintWriter(new BufferedWriter(new FileWriter(fnew)));
+            for (String info : informations) {
                 pwnew.println(info);
             }
             pwnew.close();
-        }
-        catch (IOException exception)
-        {
+        } catch (IOException exception) {
             System.out.println("Writing error " + exception.getMessage());
         }
     }
 
-       private static ArrayList<String> readXmlFile() {
-           ArrayList<String> txt = new ArrayList<String>();
-           try {
+    private static ArrayList<String> readXmlFile() {
+        ArrayList<String> txt = new ArrayList<String>();
+        try {
 
-               File file = new File("/home/sylvain/Documents/datadir/coccinellidae/eml.xml");//Need to be fix HARDOCING
-             //  File file = new File("/home/sylvain/Documents/datadir/resources/ResTest1/eml.xml");//Need to be fix HARDOCING
-               DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            File file = new File("/home/sylvain/Documents/datadir/coccinellidae/eml.xml");//Need to be fix HARDOCING
+            //  File file = new File("/home/sylvain/Documents/datadir/resources/ResTest1/eml.xml");//Need to be fix HARDOCING
+            DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 
-               Document doc = dBuilder.parse(file);
-               if (doc.hasChildNodes()) {
-                   printNode(doc.getChildNodes(),"",txt);
-               }
-           } catch (Exception e) {
-               System.out.println(e.getMessage());
-           }
-           return txt;
-       }
+            Document doc = dBuilder.parse(file);
+            if (doc.hasChildNodes()) {
+                printNode(doc.getChildNodes(), "", txt);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return txt;
+    }
 
     /**
      * Recursive method for tracking/printing everyNode
+     *
      * @param nodeList
      */
-    private static void printNode(NodeList nodeList,String position,ArrayList<String> txt) {
+    private static void printNode(NodeList nodeList, String position, ArrayList<String> txt) {
 
         for (int count = 0; count < nodeList.getLength(); count++) {
             Node tempNode = nodeList.item(count);
@@ -146,7 +148,7 @@ public class GenerateDCAT {
                 }
                 //Recursive loop
                 if (tempNode.hasChildNodes()) {
-                    if(tempNode.getChildNodes().getLength() == 1){
+                    if (tempNode.getChildNodes().getLength() == 1) {
                         System.out.println(tempNode.getTextContent());
                         txt.add(tempNode.getTextContent());
                     }
@@ -158,46 +160,90 @@ public class GenerateDCAT {
         }
     }
 
-    private String createCatalog(){
+    private String createCatalog() {
         //Mandatory
         //dct:title
         //dct:description
         //dct:publisher
         //dcat:dataset
+
+        //Recommended
         //foaf:homepage
-        //dct:rights
         //dct:issued
         //dct:modified
         //dcat:themeTaxonomy
+
+        //Optional
+        //dct:rights
         //dct:spatial
 
 
         return new String();
     }
 
-    private String createDataset(){
-        String s = "";
+    /**
+     * Create the DCAT feed for one dataset
+     *
+     * @param resource the resource to create the DCAT feed for
+     * @return string representation of DCAT feed
+     */
+    public static String createDCATDataset(Resource resource) {
 
+        StringBuilder sb = new StringBuilder();
+        Eml eml = resource.getEml();
+
+        //Base
+        String subject = eml.getHomepageUrl() + "/#dataset";
+        sb.append(subject);
+        sb.append("\n");
+
+        //----------------------------------
+        //Mandatory
         //dct:title
+        String title = eml.getTitle();
+        sb.append(title);
+        sb.append("\n");
         //dct:description
-        //dcat:theme
+        String description = eml.getDescription().toString();
+        sb.append(description);
+        sb.append("\n");
+
+        //---------------------------------
+        //Recommended
         //dcat:keyword
+        String keyword = "";
+        for (KeywordSet key : eml.getKeywords()) {
+            keyword += key.getKeywordsString();
+        }
+        sb.append(keyword);
+        sb.append("\n");
+
+        //dcat:theme
+        String theme = "";
+        //adms:contactPoint
+        String contactPoint = "";
+
+        //----------------------------------
+        //Optional
         //dct:issued
         //dct:modified
         //dct:isVersionOf
         //dct:spatial
         //adms:versionInfo
         //adms:versionNotes
-        //adms:contactPoint
         //dcat:landingPage
 
 
-        return new String();
+        return sb.toString();
     }
 
-    private String createDistribution(){
-        String plop ="";
+    private String createDistribution() {
+        String plop = "";
+
+        //Recommended
         //dct:description
+
+        //Optional
         //dct:license
         //dcat:mediaType
         //dct:format
