1a65e18e69eac69d7291e343453ca0fc5f497f59
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2023-08-22 12:01:29 +0200

    Debug logs

--- src/main/java/org/gbif/ipt/service/registry/impl/RegistryManagerImpl.java
+++ src/main/java/org/gbif/ipt/service/registry/impl/RegistryManagerImpl.java
@@ -185,7 +185,10 @@ public class RegistryManagerImpl extends BaseManager implements RegistryManager 
     data.add(new BasicNameValuePair("serviceURLs", services.serviceURLs));
 
     if (resource.getSubtype() != null) {
+      LOG.debug("Adding subtype parameter [{}]", resource.getSubtype());
       data.add(new BasicNameValuePair("subtype", resource.getSubtype()));
+    } else {
+      LOG.debug("There is no dataset subtype, nothing added");
     }
 
     return data;
