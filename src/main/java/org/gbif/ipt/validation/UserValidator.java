8b9f4cccc76395d3a0cf93325cdb517a3ae84eba
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2023-09-05 18:02:39 +0200

    #2085 Email validation - display validation errors, add translations

--- src/main/java/org/gbif/ipt/validation/UserValidator.java
+++ src/main/java/org/gbif/ipt/validation/UserValidator.java
@@ -16,7 +16,25 @@ package org.gbif.ipt.validation;
 import org.gbif.ipt.action.BaseAction;
 import org.gbif.ipt.model.User;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserValidator extends BaseValidator {
+
+  private static final Map<String, String> ERROR_TRANSLATIONS = new HashMap<>();
+
+  static {
+    ERROR_TRANSLATIONS.put("Missing final '@domain'", "validation.email.error.domain.missingFinal");
+    ERROR_TRANSLATIONS.put("Missing domain", "validation.email.error.domain.missing");
+    ERROR_TRANSLATIONS.put("Domain starts with dot", "validation.email.error.domain.startsWithDot");
+    ERROR_TRANSLATIONS.put("Domain contains control or whitespace", "validation.email.error.domain.containsControl");
+    ERROR_TRANSLATIONS.put("Domain contains illegal character", "validation.email.error.domain.containsIllegal");
+    ERROR_TRANSLATIONS.put("Domain contains dot-dot", "validation.email.error.domain.containsDotDot");
+    ERROR_TRANSLATIONS.put("Domain ends with dot", "validation.email.error.domain.endsWithDot");
+    ERROR_TRANSLATIONS.put("Missing local name", "validation.email.error.localAddress.missing");
+    ERROR_TRANSLATIONS.put("Local address contains control or whitespace", "validation.email.error.localAddress.containsControl");
+    ERROR_TRANSLATIONS.put("Local address contains illegal character", "validation.email.error.localAddress.containsIllegal");
+  }
 
   public boolean validate(BaseAction action, User user) {
     return validate(action, user, true);
@@ -26,9 +44,13 @@ public class UserValidator extends BaseValidator {
     boolean valid = true;
     if (user != null) {
       if (exists(user.getEmail())) {
-        if (!isValidEmail(user.getEmail())) {
+        ValidationResult result = checkEmailValid(user.getEmail());
+        if (!result.isValid()) {
           valid = false;
-          action.addFieldError("user.email", action.getText("validation.email.invalid"));
+          action.addFieldError(
+                  "user.email",
+                  action.getText(ERROR_TRANSLATIONS.getOrDefault(result.getMessage(), "validation.email.invalid"))
+          );
         }
       } else {
         action.addFieldError("user.email", action.getText("validation.email.required"));
