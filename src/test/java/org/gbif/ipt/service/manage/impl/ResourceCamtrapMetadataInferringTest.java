82d1bee605d96041eacf1b33eb6fe0441ecd5b16
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2023-10-25 16:29:47 +0200

    ResourceCamtrapMetadataInferringTest

---
+++ src/test/java/org/gbif/ipt/service/manage/impl/ResourceCamtrapMetadataInferringTest.java
@@ -0,0 +1,305 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.ipt.service.manage.impl;
+
+import org.gbif.ipt.model.DataSchema;
+import org.gbif.ipt.model.DataSchemaField;
+import org.gbif.ipt.model.DataSchemaFieldMapping;
+import org.gbif.ipt.model.DataSchemaMapping;
+import org.gbif.ipt.model.FileSource;
+import org.gbif.ipt.model.InferredCamtrapGeographicScope;
+import org.gbif.ipt.model.InferredCamtrapMetadata;
+import org.gbif.ipt.model.InferredCamtrapTaxonomicScope;
+import org.gbif.ipt.model.InferredCamtrapTemporalScope;
+import org.gbif.ipt.model.Resource;
+import org.gbif.ipt.model.Source;
+import org.gbif.ipt.model.TextFileSource;
+import org.gbif.ipt.model.datapackage.metadata.camtrap.Taxonomic;
+import org.gbif.ipt.service.manage.SourceManager;
+import org.gbif.utils.file.FileUtils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ResourceCamtrapMetadataInferringTest {
+
+  @Mock
+  private SourceManager sourceManagerMock;
+
+  @InjectMocks
+  private ResourceMetadataInferringServiceImpl metadataInferringService;
+
+  @DisplayName("Test metadata inferring for the resource without mappings")
+  @Test
+  public void testInferMetadataResourceWithoutMappings() {
+    Resource resource = getCamtrapResource("resource-without-mappings");
+
+    InferredCamtrapMetadata inferredMetadata = (InferredCamtrapMetadata) metadataInferringService.inferMetadata(resource);
+
+    InferredCamtrapGeographicScope geographic = inferredMetadata.getInferredGeographicScope();
+    InferredCamtrapTemporalScope temporal = inferredMetadata.getInferredTemporalScope();
+    InferredCamtrapTaxonomicScope taxonomic = inferredMetadata.getInferredTaxonomicScope();
+
+    assertAll(
+        "Inferred geographic metadata must contain an error message about missing deployments mapping",
+        () -> assertFalse(geographic.isInferred()),
+        () -> assertFalse(geographic.getErrors().isEmpty()),
+        () -> assertEquals(1, geographic.getErrors().size()),
+        () -> assertTrue(geographic.getErrors().contains("datapackagemetadata.error.noDeploymentsMapped"))
+    );
+
+    assertAll(
+        "Inferred taxonomic metadata must contain an error message about missing observation mapping",
+        () -> assertFalse(taxonomic.isInferred()),
+        () -> assertFalse(taxonomic.getErrors().isEmpty()),
+        () -> assertEquals(1, taxonomic.getErrors().size()),
+        () -> assertTrue(taxonomic.getErrors().contains("datapackagemetadata.error.noObservationsMapped"))
+    );
+
+    assertAll(
+        "Inferred temporal metadata must contain an error message about missing deployments mapping",
+        () -> assertFalse(temporal.isInferred()),
+        () -> assertFalse(temporal.getErrors().isEmpty()),
+        () -> assertEquals(1, temporal.getErrors().size()),
+        () -> assertTrue(temporal.getErrors().contains("datapackagemetadata.error.noDeploymentsMapped"))
+    );
+  }
+
+  @DisplayName("Test metadata inferring for the resource with valid mappings")
+  @Test
+  public void testMetadataInferring() throws Exception {
+    Resource resource = getCamtrapResource("regular-resource");
+    TextFileSource deploymentsFileSource = getTextFileSource("deployments", "data/deployments.txt");
+    TextFileSource observationsFileSource = getTextFileSource("observations", "data/observations.txt");
+
+    when(sourceManagerMock.rowIterator(any(FileSource.class))).thenAnswer(invocation -> {
+      FileSource source = invocation.getArgument(0);
+
+      if ("deployments".equals(source.getName())) {
+        return deploymentsFileSource.rowIterator();
+      } else if ("observations".equals(source.getName())) {
+        return observationsFileSource.rowIterator();
+      } else {
+        return null;
+      }
+    });
+
+    DataSchemaMapping deploymentsMapping = getDeploymentsMapping(deploymentsFileSource);
+    DataSchemaMapping observationsMapping = getObservationsMapping(observationsFileSource);
+    resource.addDataSchemaMapping(deploymentsMapping);
+    resource.addDataSchemaMapping(observationsMapping);
+
+    InferredCamtrapMetadata inferredMetadata = (InferredCamtrapMetadata) metadataInferringService.inferMetadata(resource);
+
+    InferredCamtrapGeographicScope geographic = inferredMetadata.getInferredGeographicScope();
+    InferredCamtrapTemporalScope temporal = inferredMetadata.getInferredTemporalScope();
+    InferredCamtrapTaxonomicScope taxonomic = inferredMetadata.getInferredTaxonomicScope();
+
+    assertAll(
+        "Inferred geographic metadata must be present and valid",
+        () -> assertTrue(geographic.isInferred()),
+        () -> assertTrue(geographic.getErrors().isEmpty()),
+        () -> assertEquals(50.69905, geographic.getMinLatitude()),
+        () -> assertEquals(3.51755, geographic.getMinLongitude()),
+        () -> assertEquals(53.40744, geographic.getMaxLatitude()),
+        () -> assertEquals(8.32994, geographic.getMaxLongitude())
+    );
+
+    assertAll(
+        "Inferred temporal metadata must be present and valid",
+        () -> assertTrue(temporal.isInferred()),
+        () -> assertTrue(temporal.getErrors().isEmpty()),
+        () -> assertEquals("Wed May 04 00:00:00 CEST 2022", temporal.getStartDate().toString()),
+        () -> assertEquals("Wed Jun 01 00:00:00 CEST 2022", temporal.getEndDate().toString())
+    );
+
+    assertAll(
+        "Inferred taxonomic metadata must be present and valid",
+        () -> assertTrue(taxonomic.isInferred()),
+        () -> assertTrue(taxonomic.getErrors().isEmpty()),
+        () -> assertEquals(4, taxonomic.getData().size()),
+        () -> assertIterableEquals(
+            Arrays.asList(
+                taxonomic("49JSC", "Ondatra zibethicus"),
+                taxonomic("4RM67", "Rattus norvegicus"),
+                taxonomic("DGP6", "Anas platyrhynchos"),
+                taxonomic("3F6VX", "Gallinula chloropus")),
+            taxonomic.getData()
+        )
+    );
+  }
+
+  private static Taxonomic taxonomic(String taxonID, String scientificName) {
+    Taxonomic taxonomic = new Taxonomic();
+    taxonomic.setTaxonID(taxonID);
+    taxonomic.setScientificName(scientificName);
+
+    return taxonomic;
+  }
+
+  private static Resource getCamtrapResource(String shortname) {
+    Resource resource = new Resource();
+    resource.setShortname(shortname);
+    resource.setCoreType("camtrap-dp");
+    resource.setSchemaIdentifier("http://rs.gbif.org/schemas/camtrap-dp");
+
+    return resource;
+  }
+
+  private static TextFileSource getTextFileSource(String name, String filePath) {
+    TextFileSource source = new TextFileSource();
+    source.setName(name);
+    source.setFile(FileUtils.getClasspathFile(filePath));
+    source.setEncoding("UTF-8");
+    source.setReadable(true);
+    source.setFieldsTerminatedBy(",");
+    source.setIgnoreHeaderLines(1);
+
+    return source;
+  }
+
+  private static DataSchemaMapping getDeploymentsMapping(Source source) {
+    return DataSchemaMapping.builder()
+        .dataSchema(
+            DataSchema.builder()
+                .name("camtrap-dp")
+                .identifier("http://rs.gbif.org/schemas/camtrap-dp")
+                .build())
+        .dataSchemaFile("deployments")
+        .source(source)
+        .fields(
+            Arrays.asList(
+                DataSchemaFieldMapping.builder()
+                    .index(0)
+                    .field(DataSchemaField.builder()
+                        .name("deploymentID")
+                        .type("string")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(3)
+                    .field(DataSchemaField.builder()
+                        .name("latitude")
+                        .type("number")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(4)
+                    .field(DataSchemaField.builder()
+                        .name("longitude")
+                        .type("number")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(6)
+                    .field(DataSchemaField.builder()
+                        .name("deploymentStart")
+                        .type("datetime")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(7)
+                    .field(DataSchemaField.builder()
+                        .name("deploymentEnd")
+                        .type("datetime")
+                        .build())
+                    .build()))
+        .build();
+  }
+
+  private static DataSchemaMapping getObservationsMapping(Source source) {
+    return DataSchemaMapping.builder()
+        .dataSchema(
+            DataSchema.builder()
+                .name("camtrap-dp")
+                .identifier("http://rs.gbif.org/schemas/camtrap-dp")
+                .build())
+        .dataSchemaFile("observations")
+        .source(source)
+        .fields(
+            Arrays.asList(
+                DataSchemaFieldMapping.builder()
+                    .index(0)
+                    .field(DataSchemaField.builder()
+                        .name("observationID")
+                        .type("string")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(1)
+                    .field(DataSchemaField.builder()
+                        .name("deploymentID")
+                        .type("string")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(4)
+                    .field(DataSchemaField.builder()
+                        .name("eventStart")
+                        .type("datetime")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(5)
+                    .field(DataSchemaField.builder()
+                        .name("eventEnd")
+                        .type("datetime")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(6)
+                    .field(DataSchemaField.builder()
+                        .name("observationLevel")
+                        .type("string")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(7)
+                    .field(DataSchemaField.builder()
+                        .name("observationType")
+                        .type("string")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(9)
+                    .field(DataSchemaField.builder()
+                        .name("taxonID")
+                        .type("string")
+                        .build())
+                    .build(),
+                DataSchemaFieldMapping.builder()
+                    .index(10)
+                    .field(DataSchemaField.builder()
+                        .name("scientificName")
+                        .type("string")
+                        .build())
+                    .build()))
+        .build();
+  }
+}
