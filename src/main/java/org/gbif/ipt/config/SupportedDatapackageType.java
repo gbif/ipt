f18d338b3d55763d3ecca9b985bd3327ad6b5bc2
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2023-11-30 10:12:16 +0100

    Reformat indents in SupportedDatapackageType

--- src/main/java/org/gbif/ipt/config/SupportedDatapackageType.java
+++ src/main/java/org/gbif/ipt/config/SupportedDatapackageType.java
@@ -20,30 +20,30 @@ import static org.gbif.ipt.config.Constants.DATA_SCHEMA_MATERIAL_DP_IDENTIFIER;
 
 public enum SupportedDatapackageType {
 
-    CAMTRAP_DP(Constants.CAMTRAP_DP, DATA_SCHEMA_CAMTRAP_DP_IDENTIFIER, "1.0"),
-    COLDP(Constants.COL_DP, DATA_SCHEMA_COLDP_IDENTIFIER, "1.0"),
-    MATERIAL_DP(Constants.MATERIAL_DP, DATA_SCHEMA_MATERIAL_DP_IDENTIFIER, "0.1"),
-    INTERACTION_DP(Constants.INTERACTION_DP, DATA_SCHEMA_INTERACTION_DP_IDENTIFIER, "0.1");
+  CAMTRAP_DP(Constants.CAMTRAP_DP, DATA_SCHEMA_CAMTRAP_DP_IDENTIFIER, "1.0"),
+  COLDP(Constants.COL_DP, DATA_SCHEMA_COLDP_IDENTIFIER, "1.0"),
+  MATERIAL_DP(Constants.MATERIAL_DP, DATA_SCHEMA_MATERIAL_DP_IDENTIFIER, "0.1"),
+  INTERACTION_DP(Constants.INTERACTION_DP, DATA_SCHEMA_INTERACTION_DP_IDENTIFIER, "0.1");
 
-    private final String name;
-    private final String identifier;
-    private final String supportedVersion;
+  private final String name;
+  private final String identifier;
+  private final String supportedVersion;
 
-    SupportedDatapackageType(String name, String identifier, String supportedVersion) {
-        this.name = name;
-        this.identifier = identifier;
-        this.supportedVersion = supportedVersion;
-    }
+  SupportedDatapackageType(String name, String identifier, String supportedVersion) {
+    this.name = name;
+    this.identifier = identifier;
+    this.supportedVersion = supportedVersion;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getIdentifier() {
-        return identifier;
-    }
+  public String getIdentifier() {
+    return identifier;
+  }
 
-    public String getSupportedVersion() {
-        return supportedVersion;
-    }
+  public String getSupportedVersion() {
+    return supportedVersion;
+  }
 }
