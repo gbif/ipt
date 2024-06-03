5c5ce1c3861558d6f9a814d6b1edd6df431da102
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2024-06-03 09:53:20 +0200

    #2434 Association a Camtrap DP resource with an existing GBIF dataset

--- src/main/java/org/gbif/ipt/service/manage/impl/ResourceManagerImpl.java
+++ src/main/java/org/gbif/ipt/service/manage/impl/ResourceManagerImpl.java
@@ -73,6 +73,7 @@ import org.gbif.ipt.model.datapackage.metadata.FrictionlessMetadata;
 import org.gbif.ipt.model.datapackage.metadata.camtrap.CamtrapContributor;
 import org.gbif.ipt.model.datapackage.metadata.camtrap.CamtrapMetadata;
 import org.gbif.ipt.model.datapackage.metadata.camtrap.Geojson;
+import org.gbif.ipt.model.datapackage.metadata.camtrap.RelatedIdentifier;
 import org.gbif.ipt.model.datapackage.metadata.camtrap.Temporal;
 import org.gbif.ipt.model.datapackage.metadata.col.ColMetadata;
 import org.gbif.ipt.model.datapackage.metadata.col.FrictionlessColMetadata;
@@ -3482,6 +3483,27 @@ public class ResourceManagerImpl extends BaseManager implements ResourceManager,
           ls.add(uuid);
         } catch (IllegalArgumentException e) {
           // skip, isn't a candidate UUID
+        }
+      }
+    } else if (resource.getDataPackageMetadata() != null
+        && resource.getDataPackageMetadata() instanceof CamtrapMetadata
+        && CAMTRAP_DP.equals(resource.getCoreType())) {
+      CamtrapMetadata metadata = (CamtrapMetadata) resource.getDataPackageMetadata();
+      List<RelatedIdentifier> relatedIdentifiers = metadata.getRelatedIdentifiers();
+      for (RelatedIdentifier identifier : relatedIdentifiers) {
+        if (identifier != null && identifier.getRelatedIdentifier() != null
+            && identifier.getRelatedIdentifier().contains("gbif")
+            && identifier.getRelatedIdentifierType() == RelatedIdentifier.RelatedIdentifierType.URL) {
+          String[] urlParts = identifier.getRelatedIdentifier().split("/");
+          if (urlParts.length > 0) {
+            String lastSegment = urlParts[urlParts.length - 1];
+            try {
+              UUID uuid = UUID.fromString(lastSegment);
+              ls.add(uuid);
+            } catch (IllegalArgumentException e) {
+              // skip, isn't a candidate UUID
+            }
+          }
         }
       }
     }
