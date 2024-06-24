3f6017b224e38b8767186665f9fe0a18c5d5a9d6
Matthew Blissett <mblissett@gbif.org>
2024-06-24 17:27:54 +0200

    Build RPMs using Oracle Linux, and automate latest release.

--- package/rpm/docker-build.sh
+++ package/rpm/docker-build.sh
@@ -2,8 +2,8 @@
 
 gitTag=$(git describe --tags --abbrev=0)
 nr_ver=$(echo $gitTag | sed s/ipt-//)
-
 echo "Building version $nr_ver"
+sed -i 's|%define nr_ver .*|%define nr_ver 3.0.6|' SPECS/ipt.spec
 
 CURRENT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 
@@ -11,25 +11,25 @@ mkdir -p $CURRENT_DIR/RPMS
 chmod 777 $CURRENT_DIR/{RPMS,SOURCES}
 
 echo
-echo "CentOS Stream8 build"
-docker pull quay.io/centos/centos:stream8
+echo "EL8 build using Oracle Linux"
+docker pull oraclelinux:8
 docker run --rm \
        -e nr_ver=$nr_ver \
        -v $CURRENT_DIR/RPMS:/root/rpmbuild/RPMS/ \
        -v $CURRENT_DIR/SOURCES:/root/rpmbuild/SOURCES/ \
        -v $CURRENT_DIR/SPECS:/root/rpmbuild/SPECS/ \
-       quay.io/centos/centos:stream8 \
+       oraclelinux:8 \
        "/root/rpmbuild/SPECS/rpm-build.sh"
 
 echo
-echo "CentOS Stream9 build"
-docker pull quay.io/centos/centos:stream9
+echo "EL9 build using Oracle Linux"
+docker pull oraclelinux:9
 docker run --rm \
        -e nr_ver=$nr_ver \
        -v $CURRENT_DIR/RPMS:/root/rpmbuild/RPMS/ \
        -v $CURRENT_DIR/SOURCES:/root/rpmbuild/SOURCES/ \
        -v $CURRENT_DIR/SPECS:/root/rpmbuild/SPECS/ \
-       quay.io/centos/centos:stream9 \
+       oraclelinux:9 \
        "/root/rpmbuild/SPECS/rpm-build.sh"
 
 chmod 755 $CURRENT_DIR/{RPMS,SOURCES}
