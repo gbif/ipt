4ae63ad658ff94c2857126cee4e1040e46b95187
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2025-05-22 13:39:26 +0200

    Add script for jenkins to build RPM only

---
+++ build/jenkins-build-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+echo
+echo
+echo "Building RPM package"
+# Run docker-build.sh, extract the line containing 'Building version', and grab the version number
+nr_ver=$(cd package/rpm && ./docker-build.sh | grep 'Building version' | sed 's/Building version //')
+
+echo "Version: $nr_ver"
+
+#echo "Uploading RPMs"
+#scp -p RPMS/noarch/ipt*.el8.noarch.rpm jenkins-deploy@apache.gbif.org:/var/www/html/packages/el8/rpm/
+#scp -p RPMS/noarch/ipt*.el9.noarch.rpm jenkins-deploy@apache.gbif.org:/var/www/html/packages/el9/rpm/
+#ssh jenkins-deploy@apache.gbif.org /var/www/html/packages/reindex
+#
+#git add SPECS/ipt.spec
+#git commit -m "Update RPM version to $nr_ver" && git push origin $(git rev-parse --abbrev-ref HEAD) || echo "Nothing changed."
