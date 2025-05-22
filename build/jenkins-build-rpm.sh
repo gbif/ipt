fdc9ae1b12d5ad649125927b2dae424ab5510a6d
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2025-05-22 20:30:25 +0200

    Debug logs for rpm file paths

--- build/jenkins-build-rpm.sh
+++ build/jenkins-build-rpm.sh
@@ -10,6 +10,10 @@ nr_ver=$(cd package/rpm && ./docker-build.sh | grep 'Building version' | sed 's/
 echo "Version: $nr_ver"
 
 echo "Uploading RPMs"
+echo "Current working directory: $(pwd)"
+echo "RPM files to upload:"
+ls -l RPMS/noarch/ipt*.rpm || echo "No RPM files found"
+
 scp -p ../package/rpm/RPMS/noarch/ipt*.el8.noarch.rpm jenkins-deploy@apache.gbif.org:/var/www/html/packages/el8/rpm/
 scp -p ../package/rpm/RPMS/noarch/ipt*.el9.noarch.rpm jenkins-deploy@apache.gbif.org:/var/www/html/packages/el9/rpm/
 ssh jenkins-deploy@apache.gbif.org /var/www/html/packages/reindex
