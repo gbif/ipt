3d74ceae9b452ebfb3c405b437b1743951223896
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2025-07-29 15:55:43 +0200

    DataPackageMappingTemplate

---
+++ src/main/java/org/gbif/ipt/model/template/DataPackageMappingTemplate.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.ipt.model.template;
+
+import org.gbif.ipt.model.DataPackageTableSchema;
+
+import java.util.Date;
+import java.util.Set;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DataPackageMappingTemplate {
+
+  private String name;
+  private String description;
+  private Set<String> keywords;
+  private Schema schema;
+  private String createdBy;
+  private Date created;
+  private String updatedBy;
+  private Date updated;
+  private Set<DataPackageTableSchema> tables;
+
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class Schema {
+    private String identifier;
+    private String name;
+    private String version;
+    private Date issuedDate;
+  }
+}
